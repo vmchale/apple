fb8e7f7eb6e4d76a7cbf4a7003d5a0f827fcd469
Vanessa McHale <vamchale@gmail.com>
2022-08-28 12:39:36 -0400

    patches for mac build

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ HS_SRC := $(shell find src -type f) $(shell find lib -type f) apple.cabal
 
 libapple.so: $(HS_SRC) include/apple.h
 	cabal build flib:apple -w ghc-9.4.2
-	cp $$(fd 'libapple\.so$$' dist-newstyle) .
+	cp $$(fd 'libapple\.dylib$$' dist-newstyle) .
 
 moddeps.svg: $(HS_SRC)
 	graphmod -i src | dot -Tsvg -o $@
@@ -18,4 +18,4 @@ install:
 
 clean:
 	make -C pyc clean
-	rm -rf dist-newstyle tags moddeps.svg *.hp *.prof *.svg *.so
+	rm -rf dist-newstyle tags moddeps.svg *.hp *.prof *.svg *.so *.dylib
--- src/Sys/DL.hs
+++ src/Sys/DL.hs
@@ -7,8 +7,6 @@ import           Foreign.C.Types                       (CSize)
 import           Foreign.Ptr                           (FunPtr, IntPtr (..), Ptr, castFunPtrToPtr, ptrToIntPtr)
 import           System.Posix.DynamicLinker.ByteString (DL, RTLDFlags (RTLD_LAZY), dlclose, dlopen, dlsym)
 
-#include <gnu/lib-names.h>
-
 mem' :: IO (Int, Int)
 mem' = do {(m,f) <- mem; pure (g m, g f)}
     where g = (\(IntPtr i) -> i) . ptrToIntPtr . castFunPtrToPtr
@@ -19,4 +17,4 @@ mem = do {c <- libc; m <- dlsym c "malloc"; f <- dlsym c "free"; dlclose c$>(m, 
 ll p = dlopen p [RTLD_LAZY]
 
 libc :: IO DL
-libc = ll {# const LIBC_SO #}
+libc = ll "libc.so"
