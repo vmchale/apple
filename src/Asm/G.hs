28cc6c57d5cc3c3a3cb6978a8de205888317bd9a
Vanessa McHale <vamchale@gmail.com>
2022-09-21 18:04:04 -0400

    alias

--- src/Asm/G.hs
+++ src/Asm/G.hs
@@ -31,7 +31,7 @@ data Mv = Mv { coal :: IS.IntSet, constr :: IS.IntSet, frz :: IS.IntSet, wl :: I
 mapWl f mv = mv { wl = f (wl mv) }
 mapActv f mv = mv { actv = f (actv mv) }
 
-data St = St { mvs :: Movs, aS :: GS, aL :: GL, mvS :: Mv, degs :: IM.IntMap Int, initial :: [Int], wkls :: Wk, stack :: [Int] }
+data St = St { mvs :: Movs, aS :: GS, aL :: GL, mvS :: Mv, degs :: IM.IntMap Int, initial :: [Int], wkls :: Wk, stack :: [Int], alias :: IM.IntMap Int }
 
 mapMv f st = st { mvS = f (mvS st) }
 mapWk f st = st { wkls = f (wkls st) }
@@ -54,7 +54,7 @@ inc = IM.alter (\k -> case k of {Nothing -> Nothing;Just d -> Just$d+1})
 -- | To be called in reverse order, init with liveOut for the block
 build :: (Arch (p (ControlAnn, NLiveness)), Copointed p) => IS.IntSet -> St -> [p (ControlAnn, NLiveness)] -> (IS.IntSet, St)
 build l st [] = (l, st)
-build l (St ml as al mv ds i wk s) (isn:isns) | isM isn =
+build l (St ml as al mv ds i wk s a) (isn:isns) | isM isn =
     let ca = fst (copoint isn)
         nl = snd (copoint isn)
         u = usesNode ca
@@ -63,7 +63,7 @@ build l (St ml as al mv ds i wk s) (isn:isns) | isM isn =
         nIx = nx nl
         ml' = thread [ kϵ @! nIx | kϵ <- IS.toList (u `IS.union` d) ] ml
         le = lm `IS.union` d
-        st' = St ml' as al (mapWl (IS.insert nIx) mv) ds i wk s
+        st' = St ml' as al (mapWl (IS.insert nIx) mv) ds i wk s a
         st'' = thread [ addEdge lϵ dϵ | lϵ <- IS.toList le, dϵ <- IS.toList d ] st'
         l' = u `IS.union` (lm IS.\\ d)
     in build l' st'' isns
@@ -72,14 +72,14 @@ build l (St ml as al mv ds i wk s) (isn:isns) | isM isn =
         u = usesNode ca
         d = defsNode ca
         le = l `IS.union` d
-        st' = St ml as al mv ds i wk s
+        st' = St ml as al mv ds i wk s a
         st'' = thread [ addEdge lϵ dϵ | lϵ <- IS.toList le, dϵ <- IS.toList d ] st'
         l' = u `IS.union` (l IS.\\ d)
     in build l' st'' isns
 
 -- TODO: Memb?
 addEdge :: Int -> Int -> St -> St
-addEdge u v st@(St ml as al mv ds i wk s) =
+addEdge u v st@(St ml as al mv ds i wk s a) =
     if (u, v) `S.notMember` as && u /= v
         then
             let as' = as `S.union` S.fromList [(u,v), (v, u)]
@@ -87,11 +87,11 @@ addEdge u v st@(St ml as al mv ds i wk s) =
                 vC = v `IS.notMember` pre wk
                 al' = (if uC then u !: v else id)$(if vC then v !: u else id) al
                 ds' = (if uC then inc u else id)$(if vC then inc v else id) ds
-            in St ml as' al' mv ds' i wk s
+            in St ml as' al' mv ds' i wk s a
         else st
 
 mkWorklist :: St -> St
-mkWorklist st@(St _ _ _ _ ds i wk _) =
+mkWorklist st@(St _ _ _ _ ds i wk _ _) =
     let i' = []
         wk' = thread [ (case () of { _ | ds IM.! n >= ᴋ -> mapSp; _ | isMR n st -> mapFr; _-> mapSimp}) (IS.insert n) | n <- i] wk
     in st { initial = i', wkls = wk' }
@@ -103,10 +103,10 @@ isMR :: Int -> St -> Bool
 isMR i st = nodeMoves i st /= IS.empty
 
 nodeMoves :: Int -> St -> IS.IntSet
-nodeMoves n (St ml _ _ mv _ _ _ _) = (ml IM.! n) `IS.intersection` (actv mv `IS.union` wl mv)
+nodeMoves n (St ml _ _ mv _ _ _ _ _) = (ml IM.! n) `IS.intersection` (actv mv `IS.union` wl mv)
 
 simplify :: St -> St
-simplify s@(St _ _ al _ ds _ wk@(Wk _ _ _ stϵ) st) | Just (n,ns) <- IS.minView stϵ =
+simplify s@(St _ _ al _ ds _ wk@(Wk _ _ _ stϵ) st _) | Just (n,ns) <- IS.minView stϵ =
     let ds' = thread [ dec m | m <- al IM.! n ] ds
     in s { wkls = wk { simp = ns }, stack = n:st, degs = ds' }
                                                    | otherwise = s
@@ -140,3 +140,6 @@ conserv is s =
     let d = degs s
         k = length (filter (\n -> (d IM.! n)>=ᴋ) is)
     in k<ᴋ
+
+getAlias :: Int -> St -> Int
+getAlias i s = case IM.lookup i (alias s) of {Just i' -> getAlias i' s; Nothing -> i}
