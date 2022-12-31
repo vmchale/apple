658afc6de49add15c15f58066a6e741cbd0336af
Vanessa McHale <vamchale@gmail.com>
2022-12-31 17:39:55 -0500

    case (asm trans)

--- src/Asm/X86/Trans.hs
+++ src/Asm/X86/Trans.hs
@@ -154,6 +154,12 @@ ir (IR.Cpy (IR.AP tD (Just e) _) (IR.AP tS (Just (IR.ConstI d)) _) (IR.ConstI n)
     i <- nextR; t <- nextR
     l <- nextL; endL <- nextL
     pure $ plE ++ [IAddRR () (IReg iR) (absReg tD), MovRI () i 0, Label () l, CmpRI () i (n32-1), Jg () endL, MovRA () t (RSD (absReg tS) Eight i d8), MovAR () (RS (IReg iR) Eight i) t, IAddRI () i 1, J () l, Label () endL]
+ir (IR.Cpy (IR.AP tD Nothing _) (IR.AP tS (Just e) _) ne) = do
+    iR <- nextI; plE <- evalE e (IR.ITemp iR)
+    nR <- nextI; plN <- evalE (IR.IB IR.IMinus ne (IR.ConstI 1)) (IR.ITemp nR)
+    i <- nextR; t <- nextR
+    l <- nextL; endL <- nextL
+    pure $ plE ++ plN ++ [IAddRR () (IReg iR) (absReg tS), MovRI () i 0, Label () l, CmpRR () i (IReg nR), Jg () endL, MovRA () t (RS (IReg iR) Eight i), MovAR () (RS (absReg tD) Eight i) t, IAddRI () i 1, J () l, Label () endL]
 -- https://www.cs.uaf.edu/2015/fall/cs301/lecture/09_23_allocation.html
 ir (IR.Sa t i)                                          = pure [ISubRI () SP (saI i+8), MovRR () (absReg t) SP]
 ir (IR.Pop i)                                           = pure [IAddRI () SP (saI i+8)]
