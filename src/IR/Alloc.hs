a1029ea48e036bc1aa074dded84a760cc9efee97
Vanessa McHale <vamchale@gmail.com>
2023-01-18 20:59:42 -0500

    push frees?

--- src/IR/Alloc.hs
+++ src/IR/Alloc.hs
@@ -12,10 +12,10 @@ import           Prettyprinter     (Doc, pretty, (<+>))
 import           Prettyprinter.Ext
 
 frees :: IM.IntMap Temp -> [Stmt] -> [Stmt]
-frees a = iF a.live
+frees a = pf.iF a.live
 
 live :: [Stmt] -> [(Stmt, Interval)]
-live  = intervals . reconstruct . IR.mkControlFlow
+live = intervals . reconstruct . IR.mkControlFlow
 
 prettyIRI :: [(Stmt, Interval)] -> Doc ann
 prettyIRI = prettyLines . fmap (\(s,i) -> pretty s <+> pretty i)
@@ -24,3 +24,8 @@ iF :: IM.IntMap Temp -> [(Stmt, Interval)] -> [Stmt]
 iF a = concatMap g where
     g (RA{}, _) = []
     g (s, i)    = s:[ Free t | t <- ts ] where ts = mapMaybe (`IM.lookup` a) (IS.toList (done i))
+
+pf :: [Stmt] -> [Stmt]
+pf (s0@J{}:s1@Free{}:s2@L{}:ss) = s0:s2:s1:pf ss
+pf (s:ss)                       = s:pf ss
+pf []                           = []
