d1c6d0a2f87d2728fa217d7e94d3dcfbe65cb04c
Vanessa McHale <vamchale@gmail.com>
2023-01-21 12:48:16 -0500

    restore xmm0-... on calling malloc

--- src/Asm/X86/Frame.hs
+++ src/Asm/X86/Frame.hs
@@ -8,22 +8,30 @@ import qualified Data.IntSet    as IS
 import           Data.Maybe     (mapMaybe)
 
 frameC :: [X86 X86Reg FX86Reg Interval] -> [X86 X86Reg FX86Reg ()]
-frameC = concat . go IS.empty
-    where go _ [] = []
-          go s (isn:isns) =
+frameC = concat . go IS.empty IS.empty
+    where go _ _ [] = []
+          go s fs (isn:isns) =
             let i = copoint isn
                 s' = s `IS.union` new i `IS.difference` done i
+                fs' = fs `IS.union` fnew i `IS.difference` fdone i
             in case isn of
                 Call _ cf ->
                     let
                         cs = handleRax cf $ mapMaybe fromInt $ IS.toList s
-                        scratch = even$length cs
+                        xms = mx cf $ mapMaybe fInt $ IS.toList fs
+                        scratch = even(length cs+length xms)
                         save = (if scratch then (++[ISubRI () Rsp 8]) else id)$fmap (Push ()) cs
                         restore = (if scratch then (IAddRI () Rsp 8:) else id)$fmap (Pop ()) (reverse cs)
-                    in (save ++ void isn : restore) : go s' isns
-                _ -> [void isn] : go s' isns
+                        savex = concatMap puxmm xms
+                        restorex = concatMap poxmm (reverse xms)
+                    in (save ++ savex ++ void isn : restorex ++ restore) : go s' fs' isns
+                _ -> [void isn] : go s' fs' isns
           handleRax Malloc = filter (/=Rax)
           handleRax Free   = id
+          puxmm xr = [ISubRI () Rsp 8, MovqAX () (R Rsp) xr]
+          poxmm xr = [MovqXA () xr (R Rsp), IAddRI () Rsp 8]
+          mx Free = const []
+          mx _    = id
 
 fromInt :: Int -> Maybe X86Reg
 fromInt 1    = Just Rsi
@@ -35,3 +43,22 @@ fromInt 6    = Just Rax
 fromInt (-1) = Just R10
 fromInt (-2) = Just R11
 fromInt _    = Nothing
+
+fInt :: Int -> Maybe FX86Reg
+fInt 8     = Just XMM0
+fInt 9     = Just XMM1
+fInt 10    = Just XMM2
+fInt 11    = Just XMM3
+fInt 12    = Just XMM4
+fInt 13    = Just XMM5
+fInt 14    = Just XMM6
+fInt 15    = Just XMM7
+fInt (-5)  = Just XMM8
+fInt (-6)  = Just XMM9
+fInt (-7)  = Just XMM10
+fInt (-8)  = Just XMM11
+fInt (-9)  = Just XMM12
+fInt (-10) = Just XMM13
+fInt (-11) = Just XMM14
+fInt (-12) = Just XMM15
+fInt _     = Nothing
