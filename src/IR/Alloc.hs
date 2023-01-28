c11fe6a9904116f3370db54a222e860802426aae
Vanessa McHale <vamchale@gmail.com>
2023-01-28 14:58:50 -0500

    whoops correctness

--- src/IR/Alloc.hs
+++ src/IR/Alloc.hs
@@ -4,6 +4,7 @@ import           CF
 import qualified Data.IntMap       as IM
 import qualified Data.IntSet       as IS
 import           Data.Maybe        (mapMaybe)
+import           Data.Tuple.Extra  (first3)
 import           IR
 import qualified IR.CF             as IR
 import           LI
@@ -26,7 +27,11 @@ iF a = concatMap g where
     g (s, i)    = s:[ Free t | t <- ts ] where ts = mapMaybe (`IM.lookup` a) (IS.toList (done i))
 
 pf :: [Stmt] -> [Stmt]
-pf (s0@J{}:s1@Free{}:s2@L{}:ss) = s0:s2:s1:pf ss
--- FIXME: j free free l ...
+pf (s@J{}:ss) | Just (fs, l, ss') <- cfs ss = s:l:fs ++ pf ss'
 pf (s:ss)                       = s:pf ss
 pf []                           = []
+
+cfs :: [Stmt] -> Maybe ([Stmt], Stmt, [Stmt])
+cfs (s@Free{}:ss) = first3 (s:) <$> cfs ss
+cfs (s@L{}:ss)    = Just ([], s, ss)
+cfs _             = Nothing
