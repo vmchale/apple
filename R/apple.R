6a74e23f6b5891d59fdd3a3cae80b654449943d0
Vanessa McHale <vamchale@gmail.com>
2023-02-12 19:15:20 -0500

    R on mac

--- R/apple.R
+++ R/apple.R
@@ -1,4 +1,4 @@
-dyn.load("/usr/local/lib/libappler.so"); .Call("hs_init_R")
+dyn.load("/usr/local/lib/libappler.dylib"); .Call("hs_init_R")
 tyof<-function(a) {.Call("ty_R",a)}
 asm<-function(a) {.Call("asm_R",a)}
 jit<-function(a) {.Call("jit_R",a)}
