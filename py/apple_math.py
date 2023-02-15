c07fa64ffcfc643fd8343b224dce0840a42624c8
Vanessa McHale <vamchale@gmail.com>
2023-02-15 17:22:44 -0500

    ay it works lol

--- py/apple_math.py
+++ py/apple_math.py
@@ -11,12 +11,14 @@ erf_apple = fs('../math/erf.🍏')
 ncdf_apple = fs('../math/ncdf.🍎')
 bessel_apple = fs('../math/bessel.🍏')
 chisqcdf_apple = fs('../math/chisqcdf.🍎')
+gamma_apple = fs('../math/gamma.🍏')
 
 regress_f=apple.cache(regress_apple)
 erf_f=apple.cache(erf_apple)
 ncdf_f=apple.cache(ncdf_apple)
 bessel_f=apple.cache(bessel_apple)
 chisqcdf_f=apple.cache(chisqcdf_apple)
+gamma_f=apple.cache(gamma_apple)
 
 def regress(x,y):
   return apple.f(regress_f,x,y)
@@ -32,3 +34,6 @@ def bessel1(a,x):
 
 def chisqcdf(k,x):
   return apple.f(chisqcdf_f,k,x)
+
+def gamma(x):
+  return apple.f(gamma_f,x)
