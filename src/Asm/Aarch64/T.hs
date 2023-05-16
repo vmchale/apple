b4e2b44fa2f7314721099e3fc6c38860473bb224
Vanessa McHale <vamchale@gmail.com>
2023-05-16 18:54:33 -0400

    faster

--- src/Asm/Aarch64/T.hs
+++ src/Asm/Aarch64/T.hs
@@ -126,11 +126,24 @@ ir (IR.Cpy (IR.AP tD Nothing _) (IR.AP tS Nothing _) eN) = do
     plEN <- eval eN (IR.ITemp rN)
     l <- nextL; eL <- nextL
     pure $ plEN ++ [MovRC () i 0, Label () l, CmpRR () i (IReg rN), Bc () Geq eL, Ldr () t (BI (absReg tS) i Three), Str () t (BI (absReg tD) i Three), AddRC () i i 1, B () l, Label () eL]
+ir (IR.Cpy (IR.AP tD (Just eD) _) (IR.AP tS (Just eS) _) (IR.ConstI n)) | (n', 0) <- n `quotRem` 2, n' <= 4 = do
+    rD <- nextI; rS <- nextI
+    t0 <- nextR; t1 <- nextR
+    plED <- eval ((IR.Reg tD)+eD) (IR.ITemp rD)
+    plES <- eval ((IR.Reg tS)+eS) (IR.ITemp rS)
+    pure $ plED ++ plES ++ concat [ [Ldp () t0 t1 (RP (IReg rS) (i*16)), Stp () t0 t1 (RP (IReg rD) (i*16))] | i <- [0..fromIntegral (n'-1)] ]
+ir (IR.Cpy (IR.AP tD (Just eD) _) (IR.AP tS (Just eS) _) (IR.ConstI n)) | (n', 1) <- n `quotRem` 2, n' <= 4 = do
+    rD <- nextI; rS <- nextI
+    t0 <- nextR; t1 <- nextR
+    plED <- eval ((IR.Reg tD)+eD) (IR.ITemp rD)
+    plES <- eval ((IR.Reg tS)+eS) (IR.ITemp rS)
+    let li=fromIntegral$(n-1)*8
+    pure $ plED ++ plES ++ concat [ [Ldp () t0 t1 (RP (IReg rS) (i*16)), Stp () t0 t1 (RP (IReg rD) (i*16))] | i <- [0..fromIntegral (n'-1)] ] ++ [Ldr () t0 (RP (IReg rS) li), Str () t0 (RP (IReg rD) li)]
 ir (IR.Cpy (IR.AP tD (Just eD) _) (IR.AP tS (Just eS) _) eN) = do
     rD <- nextI; rS <- nextI; rN <- nextI; i <- nextR
     t0 <- nextR; t1 <- nextR
-    plED <- eval (IR.IB IR.IPlus (IR.Reg tD) eD) (IR.ITemp rD)
-    plES <- eval (IR.IB IR.IPlus (IR.Reg tS) eS) (IR.ITemp rS)
+    plED <- eval ((IR.Reg tD)+eD) (IR.ITemp rD)
+    plES <- eval ((IR.Reg tS)+eS) (IR.ITemp rS)
     plEN <- eval eN (IR.ITemp rN)
     let rDA=IReg rD; rSA=IReg rS; rNA=IReg rN
     l <- nextL; eL <- nextL
