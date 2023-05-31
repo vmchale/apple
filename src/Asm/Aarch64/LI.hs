7796e660d4eb41e3e237f8d0d7e51f8fe8c05bac
Vanessa McHale <vamchale@gmail.com>
2023-05-30 21:47:51 -0400

    probably more correct

--- src/Asm/Aarch64/LI.hs
+++ src/Asm/Aarch64/LI.hs
@@ -3,14 +3,11 @@
 module Asm.Aarch64.LI ( mkIntervals ) where
 
 import           Asm.Aarch64
-import           Asm.Aarch64.B
-import           Asm.Aarch64.CF
 import           Asm.Ar
 import           Asm.L
 import           CF
 import           Class.E
 import           LI
-import           LR
 
 mkIntervals :: (E reg, E freg, Arch AArch64 reg freg) => [AArch64 reg freg ()] -> [AArch64 reg freg Interval]
 mkIntervals = intervals . enliven . mkLive
