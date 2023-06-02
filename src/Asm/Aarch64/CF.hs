c4050c04bdd7e3c2d9cf7023e99d75f6dbc7c1b6
Vanessa McHale <vamchale@gmail.com>
2023-06-02 18:11:28 -0400

    CORRECT (I think)

--- src/Asm/Aarch64/CF.hs
+++ src/Asm/Aarch64/CF.hs
@@ -56,11 +56,11 @@ udb asms = UD (uBB asms) (uBBF asms) (dBB asms) (dBBF asms)
 udd asm = UD (uses asm) (usesF asm) (defs asm) (defsF asm)
 
 uBB, dBB :: E reg => [AArch64 reg freg a] -> IS.IntSet
-uBB = fst . foldl' (\(pU, pD) n -> (pU `IS.union` (uses n IS.\\ pD), defs n `IS.union` pU)) (IS.empty, IS.empty)
+uBB = fst . foldr (\p (nU, nD) -> (uses p `IS.union` (nU IS.\\ nD), defs p `IS.union` nU)) (IS.empty, IS.empty)
 dBB = foldMap defs
 
 uBBF, dBBF :: E freg => [AArch64 reg freg a] -> IS.IntSet
-uBBF = fst . foldl' (\(pU, pD) n -> (pU `IS.union` (usesF n IS.\\ pD), defsF n `IS.union` pD)) (IS.empty, IS.empty)
+uBBF = fst . foldr (\p (nU, nD) -> (usesF p `IS.union` (nU IS.\\ nD), defsF p `IS.union` nD)) (IS.empty, IS.empty)
 dBBF = foldMap defsF
 
 defs, uses :: E reg => AArch64 reg freg a -> IS.IntSet
