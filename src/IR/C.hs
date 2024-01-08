90af142aa7268c580fa8e3a5af687b83512e5af5
Vanessa McHale <vamchale@gmail.com>
2024-01-07 19:44:11 -0500

    next: matmul B)

--- src/IR/C.hs
+++ src/IR/C.hs
@@ -26,12 +26,18 @@ cToIR :: LSt -> [CS] -> ([Stmt], WSt)
 cToIR (LSt ls ts) cs = runState (foldMapM cToIRM cs) (WSt ls ts)
 
 cToIRM :: CS -> IRM [Stmt]
-cToIRM (C.MT t e) = pure [IR.MT (ctemp t) (irE e)]
-cToIRM (C.MX t e) = pure [IR.MX (fx t) (irX e)]
-cToIRM c          = error (show c)
+cToIRM (C.MT t e)         = pure [IR.MT (ctemp t) (irE e)]
+cToIRM (C.MX t e)         = pure [IR.MX (fx t) (irX e)]
+cToIRM (C.Ma l t rnk n 8) = pure [IR.Ma l (ctemp t) (IR.IB IR.IAsl (irE rnk+irE n) 3)]
+cToIRM (C.Wr a e)         = pure [IR.Wr (irAt a) (irE e)]
+cToIRM c                  = error (show c)
+
+irAt :: ArrAcc -> AE
+irAt (ADim t rnk e l) = AP (ctemp t) (Just$IR.IB IR.IAsl (irE rnk+irE e) 3) l
 
 irE :: CE -> Exp
-irE e = error (show e)
+irE (Tmp t) = Reg (ctemp t)
+irE e       = error (show e)
 
 irX :: CFE -> FExp
 irX x = error (show x)
