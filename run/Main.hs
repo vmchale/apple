4db653ea9dd8ce2f0935a0174b2bb26acc2bcb37
Vanessa McHale <vamchale@gmail.com>
2024-01-14 19:22:58 -0500

    revE

--- run/Main.hs
+++ run/Main.hs
@@ -183,6 +183,7 @@ langHelp = liftIO $ putStr $ concat
     , lOption "˙" "at" "|" "rem"
     , lOption "@." "index of" "di." "diagonal"
     , lOption "%:" "vector mul" "odd." "parity"
+    , lOption "~" "reverse" "" ""
     ]
 
 lOption op0 desc0 op1 desc1 =
