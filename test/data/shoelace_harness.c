df0aced339c7c09312bc333c2108daacb273e5ee
Vanessa McHale <vamchale@gmail.com>
2024-04-12 10:48:42 -0400

    less indirection in example

--- test/data/shoelace_harness.c
+++ test/data/shoelace_harness.c
@@ -1,34 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define R return
-#define DO(i,n,a) {I i;for(i=0;i<n;i++){a;}}
+#include <string.h>
 
 typedef double F;typedef int64_t I; typedef void* U;
-
-typedef struct Af {I rnk; I* dim; F* xs;} Af;
-
-U poke_af (Af x) {
-    I rnk = x.rnk;
-    I t = 1;
-    DO(i,rnk,t*=x.dim[i]);
-    U p = malloc(8+8*x.rnk+8*t);
-    I* i_p = p;F* f_p = p;
-    *i_p = rnk;
-    DO(i,rnk,i_p[i+1]=x.dim[i]);
-    DO(i,t,f_p[i+1+rnk]=x.xs[i]);
-    R p;
-}
 
 extern F shoelace(U, U);
 
 int main(int argc, char *argv[]) {
     F xs[] = {0,4,4};
     F ys[] = {0,0,3};
-    I d[] = {3};
-    Af a = {1,d,xs};
-    Af b = {1,d,ys};
-    U x = poke_af(a);U y = poke_af(b);
+    U x=malloc(40);U y=malloc(40);
+    I* i_x=x;I* i_y=y;
+    *i_x=1;*i_y=1;
+    i_x[1]=3;i_y[1]=3;
+    memcpy(x+16,xs,24);
+    memcpy(y+16,ys,24);
     printf("%f\n", shoelace(x,y));
     free(x);free(y);
 }
