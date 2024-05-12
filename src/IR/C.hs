01b34c0150f6dab6e22b3f2c130c232b7350a70b
Vanessa McHale <vamchale@gmail.com>
2024-05-12 17:24:04 -0400

    one more case c->ir

--- src/IR/C.hs
+++ src/IR/C.hs
@@ -95,6 +95,7 @@ irAt (AElem t (C.ConstI rnkI) e l 8)                           = AP (ctemp t) (J
 irAt (AElem t rnk e l 8)                                       = AP (ctemp t) (Just$IR.IB IAsl (irE rnk+irE e) 3+8) l
 irAt (Raw t (C.ConstI 0) l _)                                  = AP (ctemp t) Nothing l
 irAt (Raw t o l 8)                                             = AP (ctemp t) (Just$IR.IB IAsl (irE o) 3) l
+irAt (Raw t o l 1)                                             = AP (ctemp t) (Just$irE o) l
 irAt (At dt s ix l 8) =
     let offs=foldl1 (IB IPlus) $ zipWith (\d i -> irE i*irE d) s ix
     in AP (ctemp dt) (Just$IR.IB IAsl offs 3) l
