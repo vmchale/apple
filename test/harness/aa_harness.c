05723fbae40446265f168f2235fb88356f1d148e
Vanessa McHale <vamchale@gmail.com>
2024-06-09 16:52:21 +0200

    rip

---
+++ test/harness/aa_harness.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include"../../include/apple_abi.h"
+
+extern U aa(U);
+
+int main(int argc, char *argv[]) {
+    F xs[] = {0,1,1,0};
+    I dx[] = {2,2};
+    Af a = {2,dx,xs};
+    U x = poke_af(a);
+    printf("%p\n", aa(x));
+    free(x);
+}
