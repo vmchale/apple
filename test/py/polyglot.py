299ab7adc12c6c69ee4755d7acad7d1b0af73b33
Vanessa McHale <vamchale@gmail.com>
2024-06-12 16:26:03 -0400

    insights

--- test/py/polyglot.py
+++ test/py/polyglot.py
@@ -10,7 +10,7 @@ inputs = np.array([[0,0],[0,1],[1,0],[1,1]])
 # weights and bias initialization
 hidden_weights=np.array([[0.51426693,0.56885825],[0.48725347,0.15041493]])
 hidden_bias=np.array([0.79726405,0.67601843])
-output_weights=np.array([[0.14801747],[0.37182892]])
+output_weights=np.array([0.14801747,0.37182892])
 output_bias=0.57823076
 print(hidden_weights)
 print(output_weights)
@@ -25,13 +25,12 @@ def fs(fp):
     return bs
 
 ho=fs('test/data/hiddenOutput.🍎')
-print(ho)
+# segfaults creating cif
 code=apple.jit(ho)
 hidden_layer_output=apple.f(code,hidden_weights,hidden_bias)
 print(hidden_layer_output)
 
-#  src=fs('test/data/prediction.🍎')
-#  p=apple.jit(src)
-# TODO: output_weights dim is different from numpy
-#  predicted_output=apple.f(p,hidden_weights,output_weights,hidden_bias,output_bias)
-#  print(predicted_output)
+src=fs('test/data/prediction.🍎')
+p=apple.jit(src)
+predicted_output=apple.f(p,hidden_weights,output_weights,hidden_bias,output_bias)
+print(predicted_output)
