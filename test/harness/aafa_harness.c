e5c23c4c8f9647adbd556a054fd05c41366cac68
Vanessa McHale <vamchale@gmail.com>
2024-06-13 07:23:03 -0400

    predicsh

--- test/harness/aafa_harness.c
+++ test/harness/aafa_harness.c
@@ -7,14 +7,14 @@
 extern U aafa(U,U,F);
 
 int main(int argc, char *argv[]) {
-    F wh[] = {0.51426693,0.56885825,0.48725347,0.15041493};
+    F ho[] = {0.68938893, 0.66284947, 0.78321778, 0.69560025, 0.78776923, 0.77641173, 0.8580009, 0.80143567};
     F wo[] = {0.14801747,0.37182892};
     F bo = 0.57823076;
-    I dwh[] = {2,2};
-    I dwo[] = {2};I dbh[] = {2};
-    Af a = {2,dwh,wh};
+    I dho[] = {4,2};
+    I dwo[] = {2};
+    Af a = {2,dho,ho};
     Af b = {1,dwo,wo};
     U x = poke_af(a);U y = poke_af(b);
-    paf(aafa(x,y,z,bo));
+    paf(aafa(x,y,bo));
     free(x);free(y);
 }
