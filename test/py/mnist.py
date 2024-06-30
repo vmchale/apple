a8acb86e8e55c698040a0789e99d6fdf40591eed
Vanessa McHale <vamchale@gmail.com>
2024-06-30 17:07:39 -0400

    set rand

--- test/py/mnist.py
+++ test/py/mnist.py
@@ -9,13 +9,23 @@
 #  test_image_data, test_labels = tfds.as_numpy(test)
 #  test_images = test_image_data.astype('float64')/255
 
-import apple
 import numpy as np
+
+def init(x,y):
+    return np.random.uniform(-1.,1.,(x,y))/np.sqrt(x*y)
+
+np.random.seed(17)
+
+l1=init(28*28,128)
+l2=init(128,10)
+
+import apple
 
 vize=apple.jit("λn.[?x=n,.1::float,.0]'irange 0 9 1")
 print(apple.f(vize,4))
 
-ssoftmax=apple.jit('''
+# safe softmax
+softmax=apple.jit('''
 λxs.
   { m ⟜ (⋉)/xs; a ⟜ [e:(x-m)]'xs
   ; sum ← [(+)/x]
