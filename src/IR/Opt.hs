cafb0655334d6e6cc6f9f061c60bc8c950bdb807
Vanessa McHale <vamchale@gmail.com>
2024-06-30 16:31:01 -0400

    less defective

--- src/IR/Opt.hs
+++ src/IR/Opt.hs
@@ -1,5 +1,6 @@
 module IR.Opt ( optIR ) where
 
+import           Data.Bits (shiftL)
 import           IR
 import           Op
 
@@ -7,16 +8,31 @@ optIR :: [Stmt] -> [Stmt]
 optIR = fmap opt
 
 optE :: Exp -> Exp
-optE (IB ITimes (ConstI 1) e) = optE e
-optE (IB ITimes e (ConstI 1)) = optE e
-optE (IB IPlus e (ConstI 0))  = optE e
-optE (IB op e e')             = IB op (optE e) (optE e')
-optE (IRel rel e e')          = IRel rel (optE e) (optE e')
-optE (FRel rel fe fe')        = FRel rel (optF fe) (optF fe')
-optE (IU u e)                 = IU u (optE e)
-optE (IRFloor fe)             = IRFloor (optF fe)
-optE (EAt p)                  = EAt (optP p)
-optE e                        = e
+optE (IB ITimes e0 e1) =
+    case (optE e0, optE e1) of
+        (ConstI 0, _)          -> ConstI 0
+        (_, ConstI 0)          -> ConstI 0
+        (ConstI 1, e1')        -> e1'
+        (e0', ConstI 1)        -> e0'
+        (ConstI i0, ConstI i1) -> ConstI$i0*i1
+        (e0', e1')             -> IB ITimes e0' e1'
+optE (IB IPlus e0 e1) =
+    case (optE e0, optE e1) of
+        (ConstI 0, e1')        -> e1'
+        (e0', ConstI 0)        -> e0'
+        (ConstI i0, ConstI i1) -> ConstI$i0+i1
+        (e0', e1')             -> IB IPlus e0' e1'
+optE (IB IAsl e0 e1) =
+    case (optE e0, optE e1) of
+        (ConstI i0, ConstI i1) -> ConstI$i0 `shiftL` (fromIntegral i1)
+        (e0',e1')              -> IB IAsl e0' e1'
+optE (IB op e e')            = IB op (optE e) (optE e')
+optE (IRel rel e e')         = IRel rel (optE e) (optE e')
+optE (FRel rel fe fe')       = FRel rel (optF fe) (optF fe')
+optE (IU u e)                = IU u (optE e)
+optE (IRFloor fe)            = IRFloor (optF fe)
+optE (EAt p)                 = EAt (optP p)
+optE e                       = e
 
 optF :: FExp -> FExp
 optF (FAt p) = FAt (optP p)
