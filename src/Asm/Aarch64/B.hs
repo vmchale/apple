5fbffb7416cb49d1876941b4cf59d68d51ce707c
Vanessa McHale <vamchale@gmail.com>
2024-07-01 16:24:50 -0400

    consistency?

--- src/Asm/Aarch64/B.hs
+++ src/Asm/Aarch64/B.hs
@@ -5,7 +5,8 @@ import           Asm.BB
 import           Data.List.Split (keepDelimsL, keepDelimsR, split, whenElt)
 
 bb :: [AArch64 reg freg a] -> [BB AArch64 reg freg a ()]
-bb = fmap mkBB.concatMap (split (keepDelimsL$whenElt isL)).split (keepDelimsR$whenElt cf)
-    where cf B{}=True; cf Bc{}=True; cf Ret{}=True; cf Cbnz{}=True; cf Tbnz{}=True; cf Tbz{}=True; cf BlL{}=True; cf RetL{}=True; cf _=False
+bb = filter (not.emptyBB).fmap mkBB.concatMap (split (keepDelimsL$whenElt isL)).split (keepDelimsR$whenElt cf)
+    where cf B{}=True; cf Bc{}=True; cf Cbnz{}=True; cf Tbnz{}=True; cf Tbz{}=True; cf BlL{}=True; cf RetL{}=True; cf _=False
           isL Label{}=True; isL _=False
           mkBB x = BB x ()
+          emptyBB (BB [] _)=True; emptyBB _=False
