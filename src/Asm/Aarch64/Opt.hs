53ebb7c0557b59d82d75aa4f80f200b9a3cd0adc
Vanessa McHale <vamchale@gmail.com>
2024-07-11 20:40:48 -0400

    rank+dim of v

--- src/Asm/Aarch64/Opt.hs
+++ src/Asm/Aarch64/Opt.hs
@@ -3,6 +3,7 @@ module Asm.Aarch64.Opt ( opt ) where
 import           Asm.Aarch64
 
 opt :: (Eq reg, Eq freg) => [AArch64 reg freg ()] -> [AArch64 reg freg ()]
+opt (Str _ r0 (R ar0):Str _ r1 (RP ar1 8):asms) | ar0 == ar1 = Stp () r0 r1 (R ar0):opt asms
 opt ((MovRC _ r 0):asms) = opt (ZeroR () r:asms)
 opt ((ZeroR _ r0):(MovK _ r1 u s):asms) | r0 == r1 = opt (MovZ () r1 u s:asms)
 opt ((MovRR _ r0 r1):asms) | r0 == r1 = opt asms
