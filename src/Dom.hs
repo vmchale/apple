415a8ae9b44ead02012a49fd505e1c5206bbb4b3
Vanessa McHale <vamchale@gmail.com>
2024-07-16 18:46:59 -0400

    pred

--- src/Dom.hs
+++ src/Dom.hs
@@ -1,8 +1,12 @@
 module Dom ( mkG ) where
 
 import           CF
-import qualified Data.Array as A
+import qualified Data.Array  as A
+import           Data.Graph  (transposeG)
+import qualified Data.IntMap as IM
 import           IR
 
-mkG :: (Int, [(Stmt, ControlAnn)]) -> A.Array Int [Int]
-mkG (m, ns)= A.array (0,m) ((\(_,ann) -> (node ann, conn ann))<$>ns)
+type N=Int
+
+mkG :: (Int, [(Stmt, ControlAnn)]) -> (A.Array N [N], IM.IntMap Stmt)
+mkG (m, ns)= (transposeG$A.array (0,m) ((\(_,ann) -> (node ann, conn ann))<$>ns), IM.fromList ((\(s, ann) -> (node ann, s))<$>ns))
