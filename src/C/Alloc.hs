eba45c750265ae1798f43c3d9be0c6b9cc6edfa3
Vanessa McHale <vamchale@gmail.com>
2024-07-29 12:09:50 -0400

    now vize is leaking egregiously

--- src/C/Alloc.hs
+++ src/C/Alloc.hs
@@ -17,17 +17,14 @@ live = intervals . (\(is,isns,lm) -> reconstruct is lm isns) . cfC
 
 iF :: IM.IntMap Temp -> [CS Live] -> [CS Live]
 iF a = concatMap g where
-    g RA{} = []
-    g s    = s:[ Free t | t <- ts ] where ts = mapMaybe (`IM.lookup` a) (IS.toList (done (lann s)))
+    g RA{}                  = []
+    g s@(For l _ _ _ _ cs)  = s { body = concatMap g cs }:fs l
+    g s@(While l _ _ _ cs)  = s { body = concatMap g cs }:fs l
+    g s@(For1 l _ _ _ _ cs) = s { body = concatMap g cs }:fs l
+    g s@(If l _ b0 b1)      = s { iBranch = concatMap g b0, eBranch = concatMap g b1 }:fs l
+    g s@(Ifn't l _ b)       = s { branch = concatMap g b }:fs l
+    g s@(Def _ _ cs)        = s { body = concatMap g cs }:[]
+    g s                     = s:fs (lann s)
 
-    {-
-pf :: [CS ()] -> [CS ()]
-pf (s@J{}:ss) | Just (fs, l, ss') <- cfs ss = s:l:fs ++ pf ss'
-pf (s:ss)                       = s:pf ss
-pf []                           = []
-
-cfs :: [CS ()] -> Maybe ([CS ()], CS (), [CS ()])
-cfs (s@Free{}:ss) = first3 (s:) <$> cfs ss
-cfs (s@L{}:ss)    = Just ([], s, ss)
-cfs _             = Nothing
--}
+    fs l = [ Free t | t <- ts l ]
+    ts l = mapMaybe (`IM.lookup` a) (IS.toList (done l))
