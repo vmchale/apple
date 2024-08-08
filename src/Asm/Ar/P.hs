6293f7faf7e76ec83ab409a045a754acb0e54afb
Vanessa McHale <vamchale@gmail.com>
2024-08-08 16:43:06 -0400

    stuff for consolidating moves; fmla

--- src/Asm/Ar/P.hs
+++ src/Asm/Ar/P.hs
@@ -7,12 +7,12 @@ import           Class.E
 import           Data.Copointed
 import           Data.Tuple.Extra (both)
 
-bundle :: (E reg, E freg, Copointed (arch reg freg f2), Arch arch reg freg f2)
+bundle :: (E reg, Copointed (arch reg freg f2), Arch arch reg freg f2)
        => [arch reg freg f2 ()]
        -> ([arch reg freg f2 (UD, Liveness, Maybe (Int,Int))], [arch reg freg f2 (UD, Liveness, Maybe (Int,Int))])
 bundle isns =
     let cfIsns = fmap udd isns; lIsns = mkLive isns
         mvIsns = fmap (both toInt).mI<$>isns
-        mvFIsns = fmap (both toInt).mf<$>isns
+        mvFIsns = mf<$>isns
         combine x y z = let tup = (x, copoint y, z) in tup <$ y
     in (zipWith3 combine cfIsns lIsns mvIsns, zipWith3 combine cfIsns lIsns mvFIsns)
