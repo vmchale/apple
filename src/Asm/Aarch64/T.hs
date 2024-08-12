b16ab8d10ec02f136834fab7cb84323fce70ca79
Vanessa McHale <vamchale@gmail.com>
2024-08-12 16:38:07 -0400

    simd simd
    Revert "simd simd"
    
    This reverts commit cdabbf49e26b9335cf97a2da61278701315d30e2.
    
    Revert "Revert "simd simd""
    
    This reverts commit 8cc863cd977d8b53bf0292a9e2cb2b5b3be7efe2.

--- src/Asm/Aarch64/T.hs
+++ src/Asm/Aarch64/T.hs
@@ -323,12 +323,13 @@ ir (IR.Cpy (IR.AP tD (Just (IR.ConstI oD)) _) (IR.AP tS eS _) eN) | Just uD <- m
 ir (IR.Cpy (IR.AP tD eD _) (IR.AP tS (Just (IR.ConstI oS)) _) eN) | Just uS <- mu16 oS = do
     rD <- nextI; rS <- nextI; i <- nR
     t0 <- nR; t1 <- nR
+    q0 <- nextV; q1 <- nextV
     plED <- eval (maybe id (+) eD$IR.Reg tD) (IR.ITemp rD)
     plES <- eval (IR.Reg tS) (IR.ITemp rS)
     (plEN, rN) <- plI eN
     let rDA=IReg rD; rSA=IReg rS
-    l <- nextL; eL <- nextL
-    pure $ plED ++ plES ++ plEN [Cbz () rN eL, MovRR () i rN, Tbz () rN 0 l, Ldr () t0 (RP rSA uS), Str () t0 (R rDA), SubsRC () i i 1, Bc () Eq eL, AddRC () rSA rSA 8, AddRC () rDA rDA 8, Label () l, Ldp () t0 t1 (RP rSA uS), Stp () t0 t1 (R rDA), AddRC () rSA rSA 16, AddRC () rDA rDA 16, SubsRC () i i 2, Bc () Neq l, Label () eL]
+    l <- nextL; l2 <- nextL; eL <- nextL
+    pure $ plED ++ plES ++ plEN [Cbz () rN eL, MovRR () i rN, Tbz () rN 0 l2, Ldr () t0 (RP rSA uS), Str () t0 (R rDA), SubsRC () i i 1, Bc () Eq eL, AddRC () rSA rSA 8, AddRC () rDA rDA 8, Label () l2, Tbz () rN 1 l, Ldp () t0 t1 (RP rSA uS), Stp () t0 t1 (R rDA), SubsRC () i i 2, Bc () Eq eL, AddRC () rSA rSA 16, AddRC () rDA rDA 16, Label () l, Ldp2 () q0 q1 (RP rSA uS), Stp2 () q0 q1 (R rDA), AddRC () rSA rSA 32, AddRC () rDA rDA 32, SubsRC () i i 4, Bc () Neq l, Label () eL]
 ir (IR.Cpy (IR.AP tD eD _) (IR.AP tS (Just (IR.IB Op.IPlus eS (IR.ConstI oS))) _) eN) | Just uS <- mu16 oS = do
     rD <- nextI; rS <- nextI; i <- nR
     t0 <- nR; t1 <- nR
