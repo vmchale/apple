ddc5f58e463b71346589313c4986cde262f1088c
Vanessa McHale <vamchale@gmail.com>
2024-08-12 09:15:54 -0400

    fm?

--- src/C/Trans.hs
+++ src/C/Trans.hs
@@ -1600,6 +1600,9 @@ feval (EApp _ (EApp _ (Builtin _ A.R) (FLit _ 0)) e1) t = do
 feval (EApp _ (EApp _ (Builtin _ A.R) e0) e1) t = do
     (plE0,e0e) <- plD e0; (plE1, e1e) <- plD e1
     pure $ plE0 $ plE1 [FRnd () t, MX () t ((e1e-e0e)*FTmp t+e0e)]
+feval (EApp _ (EApp _ (Builtin _ Plus) (EApp _ (EApp _ (Builtin _ Times) e0) e1)) e2) t = do
+    (pl0,t0) <- plF e0; (pl1,t1) <- plF e1; (pl2,t2) <- plF e2
+    pure $ pl0 $ pl1 $ pl2 [MX () t (FTmp t0*FTmp t1+FTmp t2)]
 feval (EApp _ (EApp _ (Builtin _ Plus) e0) (EApp _ (EApp _ (Builtin _ Times) e1) e2)) t = do
     (pl0,t0) <- plF e0; (pl1,t1) <- plF e1; (pl2,t2) <- plF e2
     pure $ pl0 $ pl1 $ pl2 [MX () t (FTmp t0+FTmp t1*FTmp t2)]
