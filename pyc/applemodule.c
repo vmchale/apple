8a4c1591a3ffed34d44344e60d5067e4ffac9b51
Vanessa McHale <vamchale@gmail.com>
2024-10-03 11:52:28 -0700

    shave

--- pyc/applemodule.c
+++ pyc/applemodule.c
@@ -5,7 +5,7 @@
 #include"../include/apple_abi.h"
 #include"../c/ffi.c"
 
-typedef void* U;typedef PyObject* PO;typedef PyArrayObject* NPA;typedef size_t S;
+typedef void* U;typedef PyObject* PY;typedef PyArrayObject* NPA;typedef size_t S;
 
 #define R return
 #define Sw switch
@@ -16,14 +16,13 @@ typedef void* U;typedef PyObject* PO;typedef PyArrayObject* NPA;typedef size_t S
 
 // https://numpy.org/doc/stable/reference/c-api/array.html
 U f_npy(const NPA o) {
+    CT(o,'d',"Error: expected an array of floats")
     J rnk=PyArray_NDIM(o);
     npy_intp* dims=PyArray_DIMS(o);
     J n=PyArray_SIZE(o);
-    J sz_i=1+rnk+n;
-    CT(o,'d',"Error: expected an array of floats")
-    S sz=sz_i*8;
-    U x=malloc(sz);J* x_i=x; F* x_f=x;
-    x_i[0]=rnk;
+    J sz_i=1+rnk+n;S sz=sz_i*8;
+    U x=malloc(sz);J* x_i=x;x_i[0]=rnk;
+    F* x_f=x;
     DO(i,rnk,x_i[i+1]=(J)dims[i]);
     U data=PyArray_DATA(o);
     memcpy(x_f+rnk+1,data,n*8);
@@ -31,103 +30,98 @@ U f_npy(const NPA o) {
 }
 
 U i_npy(NPA o) {
+    CT(o,'l',"Error: expected an array of 64-bit integers")
     J rnk=PyArray_NDIM(o);
     npy_intp* dims=PyArray_DIMS(o);
     J n=PyArray_SIZE(o);
-    J sz_i=1+rnk+n;
-    S sz=sz_i*8;
-    CT(o,'l',"Error: expected an array of 64-bit integers")
-    U x=malloc(sz);J* x_i=x;
-    x_i[0]=rnk;
+    J sz_i=1+rnk+n;S sz=sz_i*8;
+    U x=malloc(sz);J* x_i=x;x_i[0]=rnk;
     DO(i,rnk,x_i[i+1]=(J)dims[i]);
     U data=PyArray_DATA(o);
     memcpy(x_i+rnk+1,data,n*8);
     R x;
 }
 
-PyObject* npy_i(U x) {
-    J* i_p=x;
+PY npy_i(U x) {
+    J* i_p=x;J rnk=i_p[0];
     J t=1;
-    J rnk=i_p[0];
     npy_intp* dims=malloc(sizeof(npy_intp)*rnk);
     DO(i,rnk,t*=i_p[i+1];dims[i]=(npy_intp)i_p[i+1]);
     S sz=8*t;
     U data=malloc(sz);
-    // TODO figure out how to do this without copying
     memcpy(data,i_p+rnk+1,sz);
-    PyObject* res=PyArray_SimpleNewFromData(rnk,dims,NPY_INT64,data);
-    PyArray_ENABLEFLAGS((PyArrayObject*)res,NPY_ARRAY_OWNDATA);
+    PY res=PyArray_SimpleNewFromData(rnk,dims,NPY_INT64,data);
+    PyArray_ENABLEFLAGS((NPA*)res,NPY_ARRAY_OWNDATA);
     free(x);R res;
 }
 
-PyObject* npy_f(U x) {
-    J* i_p=x;
+PY npy_f(U x) {
+    J* i_p=x;J rnk=i_p[0];
     J t=1;
-    J rnk=i_p[0];
     npy_intp* dims=malloc(sizeof(npy_intp)*rnk);
     DO(i,rnk,t*=i_p[i+1];dims[i]=(npy_intp)i_p[i+1]);
     S sz=8*t;
     U data=malloc(sz);
     memcpy(data,i_p+rnk+1,sz);
-    PyObject* res=PyArray_SimpleNewFromData(rnk,dims,NPY_FLOAT64,data);
-    PyArray_ENABLEFLAGS((PyArrayObject*)res,NPY_ARRAY_OWNDATA);
+    PY res=PyArray_SimpleNewFromData(rnk,dims,NPY_FLOAT64,data);
+    PyArray_ENABLEFLAGS((NPA*)res,NPY_ARRAY_OWNDATA);
     free(x);R res;
 }
 
 void freety(FnTy* x){free(x->args);free(x);}
 
-static PyObject* apple_typeof(PyObject* self, PyObject *args) {
+static PY apple_typeof(PY self, PY args) {
     const char* inp;
     PyArg_ParseTuple(args, "s", &inp);
     char* err;char** err_p = &err;
     char* res = apple_printty(inp,err_p);
     ERR(res,err);
-    PyObject* pyres = PyUnicode_FromString(res);
-    free(res); R pyres;
+    PY py = PyUnicode_FromString(res);
+    free(res);R py;
 }
 
-static PyObject* apple_asm(PyObject* self, PyObject *args) {
+static PY apple_asm(PY self, PY args) {
     const char* inp;
     PyArg_ParseTuple(args, "s", &inp);
     char* err;char** err_p = &err;
     char* res = apple_dumpasm(inp,err_p);
     ERR(res,err);
-    PyObject* pyres = PyUnicode_FromString(res);
-    free(res); R pyres;
+    PY py = PyUnicode_FromString(res);
+    free(res);R py;
 }
 
-static PyObject* apple_ir(PyObject* self, PyObject *args) {
+static PY apple_ir(PY self, PY args) {
     const char* inp;
     PyArg_ParseTuple(args, "s", &inp);
     char* err;char** err_p = &err;
     char* res = apple_dumpir(inp,err_p);
     ERR(res,err);
-    PyObject* pyres = PyUnicode_FromString(res);
-    free(res); R pyres;
+    PY py = PyUnicode_FromString(res);
+    free(res); R py;
 }
 
-typedef struct Cache {
+typedef struct JO {
     PyObject_HEAD
     U bc;S c_sz;FnTy* ty; U sa;ffi_cif* ffi;
-} Cache;
+} JO;
 
-static void cache_dealloc(Cache* self) {
+static void cache_dealloc(JO* self) {
     munmap(self->bc,self->c_sz);
     free(self->sa);freety(self->ty);free(self->ffi);
 }
 
-static PyTypeObject CacheType = {
+static PyTypeObject JOT = {
     PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "Cache",
-    .tp_doc = PyDoc_STR("Cached JIT function"),
-    .tp_basicsize = sizeof(Cache),
+    .tp_name = "Jit Object",
+    .tp_doc = PyDoc_STR("JIT-compiled function in-memory"),
+    .tp_basicsize = sizeof(JO),
     .tp_itemsize = 0,
     .tp_flags = Py_TPFLAGS_DEFAULT,
     .tp_new = PyType_GenericNew,
     .tp_dealloc = (destructor)cache_dealloc,
 };
 
-static PyObject* apple_jit(PyObject *self, PyObject *args) {
+static PY apple_jit(PY self, PY args) {
     const char* inp;
     PyArg_ParseTuple(args, "s", &inp);
     char* err;char** err_p=&err;
@@ -135,27 +129,28 @@ static PyObject* apple_jit(PyObject *self, PyObject *args) {
     ERR(ty,err);
     U fp;S f_sz;U s;
     fp=apple_compile(&sys,inp,&f_sz,&s);
-    Cache* cc=PyObject_New(Cache, &CacheType);
+    JO* cc=PyObject_New(JO, &JOT);
     ffi_cif* ffi=apple_ffi(ty);
     cc->bc=fp;cc->c_sz=f_sz;cc->ty=ty;cc->sa=s;cc->ffi=ffi;
     Py_INCREF(cc);
-    R (PyObject*)cc;
+    R (PY)cc;
 }
 
 // file:///usr/share/doc/libffi8/html/The-Basics.html
-static PyObject* apple_f(PyObject* self, PyObject* args) {
-    Cache* c;PO arg0=NULL;PO arg1=NULL;PO arg2=NULL;PO arg3=NULL;PO arg4=NULL;PO arg5=NULL;
+static PY apple_f(PY self, PY args) {
+    JO* c;PY arg0=NULL;PY arg1=NULL;PY arg2=NULL;PY arg3=NULL;PY arg4=NULL;PY arg5=NULL;
     PyArg_ParseTuple(args, "O|OOOOOO", &c, &arg0, &arg1, &arg2, &arg3, &arg4, &arg5);
     FnTy* ty=c->ty;U fp=c->bc;
-    PO r;
+    PY r;
     ffi_cif* cif=c->ffi;
     int argc=ty->argc;
     U* vals=alloca(sizeof(U)*argc);U ret=alloca(8);
-    PO pyarg;PO pyargs[]={arg0,arg1,arg2,arg3,arg4,arg5};
+    PY pyarg;PY pyargs[]={arg0,arg1,arg2,arg3,arg4,arg5};
     for(int k=0;k<argc;k++){
         pyarg=pyargs[k];
         if(pyarg!=NULL){
             Sw(ty->args[k]){
+                // FIXME: i_npy leaks memory
                 C IA: {U* x=alloca(sizeof(U));x[0]=i_npy((NPA)pyarg);vals[k]=x;};BR
                 C FA: {U* x=alloca(sizeof(U));x[0]=f_npy((NPA)pyarg);vals[k]=x;};BR
                 C I_t: {J* xi=alloca(sizeof(J));xi[0]=PyLong_AsLong(pyarg);vals[k]=xi;};BR
