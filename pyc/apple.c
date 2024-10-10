95a94c9942bcc60ec55e8504ebbc247eb7c86414
Vanessa McHale <vamchale@gmail.com>
2024-10-10 20:15:20 +0200

    ok one of these changes is actually good

--- pyc/apple.c
+++ pyc/apple.c
@@ -5,15 +5,17 @@
 #include"../include/apple_abi.h"
 #include"../c/ffi.c"
 
-typedef void* U;typedef PyObject* PY;typedef PyArrayObject* NPA;typedef size_t S;
+typedef void* U;typedef PyObject* PY;typedef PyArrayObject* NP;typedef size_t S;
 
 #define CT(o,c,s) {PyArray_Descr *d=PyArray_DESCR(o);if(!(d->type==c)){PyErr_SetString(PyExc_RuntimeError,s);}}
-#define CD(t,rnk,x,dims) J* i_p=x;J rnk=i_p[0];npy_intp* dims=malloc(sizeof(npy_intp)*rnk);DO(i,rnk,t*=i_p[i+1];dims[i]=(npy_intp)i_p[i+1]);
-#define A(r,n,sz,x,py) J r=PyArray_NDIM(py);J n=PyArray_SIZE(py);U x=malloc(sz);{npy_intp* dims=PyArray_DIMS(py);J* x_i=x;x_i[0]=r;DO(i,r,x_i[i+1]=(J)dims[i]);}
-#define ERR(p,msg) {if(p==NULL){PyErr_SetString(PyExc_RuntimeError, msg);free(msg);R NULL;};}
+#define CD(rnk,x,t,ds) J* i_p=x;J rnk=i_p[0];npy_intp* ds=malloc(sizeof(npy_intp)*rnk);J t=1;DO(i,rnk,t*=i_p[i+1];ds[i]=(npy_intp)i_p[i+1]);
+#define AD(r,x,py) {J* x_i=x;x_i[0]=r;npy_intp* ds=PyArray_DIMS(py);DO(i,r,x_i[i+1]=(J)ds[i]);}
+#define A(r,n,sz,x,py) J r=PyArray_NDIM(py);J n=PyArray_SIZE(py);U x=malloc(sz);AD(r,x,py);
+#define ERR(p,msg) {if(p==NULL){PyErr_SetString(PyExc_RuntimeError,msg);free(msg);R NULL;};}
+#define O(pya) PyArray_ENABLEFLAGS((NP*)pya,NPY_ARRAY_OWNDATA)
 
 // https://numpy.org/doc/stable/reference/c-api/array.html
-U f_npy(const NPA o) {
+U f_npy(const NP o) {
     CT(o,'d',"Error: expected an array of floats")
     A(rnk,n,(1+rnk+n)*8,x,o);
     F* x_f=x;
@@ -22,7 +24,7 @@ U f_npy(const NPA o) {
     R x;
 }
 
-U b_npy(NPA o) {
+U b_npy(NP o) {
     CT(o,'?',"Error: expected an array of booleans")
     A(rnk,n,(1+rnk)*8+n,x,o);
     B* x_p=x;
@@ -31,7 +33,7 @@ U b_npy(NPA o) {
     R x;
 }
 
-U i_npy(NPA o) {
+U i_npy(NP o) {
     CT(o,'l',"Error: expected an array of 64-bit integers")
     A(rnk,n,(1+rnk+n)*8,x,o);
     J* x_i=x;
@@ -41,36 +43,32 @@ U i_npy(NPA o) {
 }
 
 PY npy_i(U x) {
-    J t=1;
-    CD(t,rnk,x,dims);
+    CD(rnk,x,t,dims);
     S sz=8*t;
     U data=malloc(sz);
     memcpy(data,i_p+rnk+1,sz);
-    PY res=PyArray_SimpleNewFromData(rnk,dims,NPY_INT64,data);
-    PyArray_ENABLEFLAGS((NPA*)res,NPY_ARRAY_OWNDATA);
+    PY res=PyArray_SimpleNewFromData(rnk,dims,NPY_INT64,data);O(res);
     free(x);R res;
 }
 
 PY npy_f(U x) {
-    J t=1;
-    CD(t,rnk,x,dims);
+    CD(rnk,x,t,dims);
     S sz=8*t;
     U data=malloc(sz);
     memcpy(data,i_p+rnk+1,sz);
-    PY res=PyArray_SimpleNewFromData(rnk,dims,NPY_FLOAT64,data);
-    PyArray_ENABLEFLAGS((NPA*)res,NPY_ARRAY_OWNDATA);
-    free(x);R res;
+    free(x);
+    PY res=PyArray_SimpleNewFromData(rnk,dims,NPY_FLOAT64,data);O(res);
+    R res;
 }
 
 PY npy_b(U x) {
-    J t=1;
-    CD(t,rnk,x,dims);
+    CD(rnk,x,t,dims);
     U data=malloc(t);
     B* x_p=x;
     memcpy(data,x_p+rnk*8+8,t);
-    PY res=PyArray_SimpleNewFromData(rnk,dims,NPY_BOOL,data);
-    PyArray_ENABLEFLAGS((NPA*)res,NPY_ARRAY_OWNDATA);
-    free(x);R res;
+    free(x);
+    PY res=PyArray_SimpleNewFromData(rnk,dims,NPY_BOOL,data);O(res);
+    R res;
 }
 
 void freety(FnTy* x){free(x->args);free(x);}
@@ -130,9 +128,9 @@ static PY apple_call(PY self, PY args, PY kwargs) {
         pyarg=pyargs[k];
         if(pyarg!=NULL){
             Sw(ty->args[k]){
-                C(IA,U* x=alloca(sizeof(U));x[0]=i_npy((NPA)pyarg);fs|=1<<k;vals[k]=x;)
-                C(BA,U* x=alloca(sizeof(U));x[0]=b_npy((NPA)pyarg);fs|=1<<k;vals[k]=x;)
-                C(FA,U* x=alloca(sizeof(U));x[0]=f_npy((NPA)pyarg);fs|=1<<k;vals[k]=x;)
+                C(IA,U* x=alloca(sizeof(U));x[0]=i_npy((NP)pyarg);fs|=1<<k;vals[k]=x;)
+                C(BA,U* x=alloca(sizeof(U));x[0]=b_npy((NP)pyarg);fs|=1<<k;vals[k]=x;)
+                C(FA,U* x=alloca(sizeof(U));x[0]=f_npy((NP)pyarg);fs|=1<<k;vals[k]=x;)
                 C(I_t,J* xi=alloca(sizeof(J));xi[0]=PyLong_AsLong(pyarg);vals[k]=xi;)
                 C(F_t,F* xf=alloca(sizeof(F));xf[0]=PyFloat_AsDouble(pyarg);vals[k]=xf;)
             }
