e1ea7ec143dc48f3d3f5d73630ac36f754599adb
Vanessa McHale <vamchale@gmail.com>
2024-10-11 13:16:31 +0200

    from file

--- R/test.R
+++ R/test.R
@@ -1,16 +1,15 @@
 source("./apple.R")
 library(readr)
 
-ncdf_str<-read_file("../math/ncdf.🍎")
-ncdf<-jit(ncdf_str)
+lafile<-function(f){str<-read_file(f);jit(str)}
+
+ncdf<-lafile("../math/ncdf.🍎")
 run(ncdf,3);pnorm(3)
 
-chisqcdf_str<-read_file("../math/chisqcdf.🍎")
-chisqcdf<-jit(chisqcdf_str)
+chisqcdf<-lafile("../math/chisqcdf.🍎")
 run(chisqcdf,2,2);pchisq(2,2)
 
-tcdf_str<-read_file("../math/tcdf.🍎")
-tcdf<-jit(tcdf_str)
+tcdf<-lafile("../math/tcdf.🍎")
 run(tcdf,2,12);pt(2,12)
 
 sliding_mean<-jit("([((+)/x)%ℝ(:x)]\\`7)")
@@ -23,8 +22,7 @@ any<-jit("λbs. (∨)/ₒ #f bs :: bool")
 stopifnot(run(any,c(FALSE,FALSE,FALSE,TRUE)))
 
 isbn<-jit('λxs. ((+)/ (*)`xs (}:(cyc. ⟨1,3::int⟩ 7)))|10=0')
-stopifnot(run(isbn,as.integer(c(9,7,8,0,5,9,6,5,2,8,1,2,6))))
-stopifnot(!run(isbn,as.integer(c(9,7,8,1,7,8,8,3,9,9,0,8,3))))
+stopifnot(run(isbn,as.integer(c(9,7,8,0,5,9,6,5,2,8,1,2,6))));stopifnot(!run(isbn,as.integer(c(9,7,8,1,7,8,8,3,9,9,0,8,3))))
 
 prime_mask<-jit("λN. (λn.¬((∨)/ₒ #f ([(n|x)=0]'(⍳ 2 (⌊(√(ℝn))) 1))))'(irange 2 N 1)")
 stopifnot(all(run(prime_mask,9)==c(TRUE,TRUE,FALSE,TRUE,FALSE,TRUE,FALSE,FALSE)))
