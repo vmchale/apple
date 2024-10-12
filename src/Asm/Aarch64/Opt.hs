def3020c2b86b87e4436b4bccd6b1c718f75db33
Vanessa McHale <vamchale@gmail.com>
2024-10-12 21:07:24 +0200

    hm

--- src/Asm/Aarch64/Opt.hs
+++ src/Asm/Aarch64/Opt.hs
@@ -4,7 +4,6 @@ import           Asm.Aarch64
 
 opt :: (Eq reg, Eq freg) => [AArch64 reg freg f2reg ()] -> [AArch64 reg freg f2reg ()]
 opt (Str _ r0 (R ar0):Str _ r1 (RP ar1 8):asms) | ar0 == ar1 = Stp () r0 r1 (R ar0):opt asms
-opt (SubRC _ r r1 w:CmpRC _ r' 0:asms) | r==r' = SubsRC () r r1 w:opt asms
 opt ((MovRC _ r 0):asms) = opt (ZeroR () r:asms)
 opt ((ZeroR _ r0):(MovK _ r1 u s):asms) | r0 == r1 = opt (MovZ () r1 u s:asms)
 opt ((MovRR _ r0 r1):asms) | r0 == r1 = opt asms
