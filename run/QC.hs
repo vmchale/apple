a9857ea327fbcc64134d38e41f149891e58f0855
Vanessa McHale <vamchale@gmail.com>
2024-10-15 17:22:08 -0400

    cases

--- run/QC.hs
+++ run/QC.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module QC ( gas, Val (..), freeP ) where
 
 import           A
@@ -8,7 +10,7 @@ import           Data.Functor               (($>))
 import           Data.Int                   (Int64)
 import qualified Data.IntMap                as IM
 import           Foreign.C.Types            (CDouble (..))
-import           Foreign.LibFFI             (Arg, argCDouble, argInt64, argPtr)
+import           Foreign.LibFFI             (Arg, argCDouble, argInt64, argPtr, argWord8)
 import           Foreign.Marshal.Alloc      (free, mallocBytes)
 import           Foreign.Ptr                (Ptr)
 import           Foreign.Storable           (poke, sizeOf)
@@ -50,22 +52,24 @@ gg (SVar (Nm _ (U n) _)) = do
         Nothing -> do {r <- lift rnk; ds <- lift$vectorOf (fromIntegral r) dim; modify (mapS (IM.insert n (r,ds))) $> (r,ds)}
         Just s  -> pure s
 
-data ValP = ArrDp !(Ptr (Apple Double)) | ArrIp !(Ptr (Apple Int64))
+data ValP = ArrDp !(Ptr (Apple Double)) | ArrIp !(Ptr (Apple Int64)) | ArrBp !(Ptr (Apple AB))
 
 freeP :: ValP -> IO ()
-freeP (ArrDp a) = free a
-freeP (ArrIp a) = free a
+freeP (ArrDp a) = free a; freeP (ArrIp a) = free a; freeP (ArrBp a) = free a
 
 gas :: [T a] -> IO [(Arg, Val, Maybe ValP)]
 gas = flip evalStateT (RSubst IM.empty IM.empty).traverse ga
 
-data Val = ArrD !(Apple Double) | AI !(Apple Int64) | II !Int64 | D !Double
+data Val = ArrD !(Apple Double) | AI !(Apple Int64) | AB !(Apple AB) | II !Int64 | D !Double | BB !Bool
 
 instance Pretty Val where
-    pretty (ArrD a) = pretty a
-    pretty (AI a)   = pretty a
-    pretty (II i)   = pretty i
-    pretty (D d)    = pretty d
+    pretty (ArrD a)   = pretty a
+    pretty (AI a)     = pretty a
+    pretty (AB a)     = pretty a
+    pretty (II i)     = pretty i
+    pretty (D d)      = pretty d
+    pretty (BB True)  = "#t"
+    pretty (BB False) = "#f"
 
 ga :: T a -> StateT RSubst IO (Arg, Val, Maybe ValP)
 ga (Arr sh A.F) = do
@@ -80,12 +84,30 @@ ga (Arr sh A.I) = do
     put st'
     p <- lift $ mallocBytes (sizeOf a)
     lift (poke p a $> (argPtr p, AI a, Just (ArrIp p)))
+ga (Arr sh A.B) = do
+    st <- get
+    (a, st') <- lift $ generate $ runStateT (gB sh) st
+    put st'
+    p <- lift $ mallocBytes (sizeOf a)
+    lift (poke p a $> (argPtr p, AB a, Just (ArrBp p)))
 ga I = do
     i <- lift $ generate chooseAny
     pure (argInt64 i, II i, Nothing)
 ga A.F = do
     x <- lift $ generate chooseAny
     pure (argCDouble (CDouble x), D x, Nothing)
+ga B = do
+    b <- lift $ generate chooseAny
+    pure (argWord8 (if b then 1 else 0), BB b, Nothing)
+
+b8 b = if b then Hs.A.T else Hs.A.F
+
+gB :: Sh a -> ShM (Apple AB)
+gB sh = do
+    (r,ds) <- gg sh
+    let n=fromIntegral$product ds
+    es <- lift$map b8<$>vectorOf n chooseAny
+    pure (AA r ds es)
 
 gI :: Sh a -> ShM (Apple Int64)
 gI sh = do
