dbed76b1926f0939fc2d13a55b45166dcc64d92a
Vanessa McHale <vamchale@gmail.com>
2024-10-18 18:05:02 -0400

    help

--- src/C/Trans.hs
+++ src/C/Trans.hs
@@ -214,14 +214,14 @@ for t = if ne t then For1 () else For (); for1 t = if n1 t then For1 () else For
 forc t = if nec t then For1 () else For ()
 fors t = if nee t then For1 () else For ()
 
-r21 (Arr (Ix _ i `Cons` Nil) _) | odd i = \tϵ c ss _ -> R2ofE () tϵ c ss
-                                | even i = R2ofO ()
-r21 _                           = R2of ()
-
 f21o (Arr (Ix _ i `Cons` Nil) _) | odd i = \tϵ el c eu ss _ -> F2orE () tϵ el c eu ss
                                  | even i = F2orO ()
 f21o _                           = F2or ()
 
+
+r2of ty | to ty = R2ofO ()
+        | te ty = \tϵ c ss _ -> R2ofE () tϵ c ss
+        | otherwise = R2of ()
 
 f2or ty | to ty = F2orO ()
         | te ty = \tϵ el c eu ss _ -> F2orE () tϵ el c eu ss
