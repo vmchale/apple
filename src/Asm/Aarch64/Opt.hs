0f05bca3f3a7e09579ddbae42cb99242c2cd37da
Vanessa McHale <vamchale@gmail.com>
2024-10-19 23:13:02 -0400

    big immediates

--- src/Asm/Aarch64/Opt.hs
+++ src/Asm/Aarch64/Opt.hs
@@ -3,13 +3,13 @@ module Asm.Aarch64.Opt ( opt ) where
 import           Asm.Aarch64
 
 opt :: (Eq reg, Eq freg) => [AArch64 reg freg ()] -> [AArch64 reg freg ()]
-opt (AddRC _ r0 r0' 0:asms) | r0==r0' = opt asms
+opt (AddRC _ r0 r0' 0 _:asms) | r0==r0' = opt asms
 opt (SubRC _ r0 r0' 0:asms) | r0==r0' = opt asms
-opt (Ldr x r0 (R ar):AddRC _ r1 r2 u:asms) | ar==r1&&r1==r2 = Ldr x r0 (Po ar (fromIntegral u)):opt asms
-opt (LdrD x d0 (R ar):AddRC _ r1 r2 u:asms) | ar==r1&&r1==r2 = LdrD x d0 (Po ar (fromIntegral u)):opt asms
-opt (LdrS x q0 (R ar):AddRC _ r1 r2 u:asms) | ar==r1&&r1==r2 = LdrS x q0 (Po ar (fromIntegral u)):opt asms
-opt (StrD x d0 (R ar):AddRC _ r1 r2 u:asms) | ar==r1&&r1==r2 = StrD x d0 (Po ar (fromIntegral u)):opt asms
-opt (StrS x q0 (R ar):AddRC _ r1 r2 u:asms) | ar==r1&&r1==r2 = StrS x q0 (Po ar (fromIntegral u)):opt asms
+opt (Ldr x r0 (R ar):AddRC _ r1 r2 u IZero:asms) | ar==r1&&r1==r2 = Ldr x r0 (Po ar (fromIntegral u)):opt asms
+opt (LdrD x d0 (R ar):AddRC _ r1 r2 u IZero:asms) | ar==r1&&r1==r2 = LdrD x d0 (Po ar (fromIntegral u)):opt asms
+opt (LdrS x q0 (R ar):AddRC _ r1 r2 u IZero:asms) | ar==r1&&r1==r2 = LdrS x q0 (Po ar (fromIntegral u)):opt asms
+opt (StrD x d0 (R ar):AddRC _ r1 r2 u IZero:asms) | ar==r1&&r1==r2 = StrD x d0 (Po ar (fromIntegral u)):opt asms
+opt (StrS x q0 (R ar):AddRC _ r1 r2 u IZero:asms) | ar==r1&&r1==r2 = StrS x q0 (Po ar (fromIntegral u)):opt asms
 opt (Str _ r0 (R ar0):Str _ r1 (RP ar1 8):asms) | ar0 == ar1 = Stp () r0 r1 (R ar0):opt asms
 opt (SubRC _ r r1 w:CmpRC _ r' 0:asms) | r==r' = SubsRC () r r1 w:opt asms
 opt ((MovRC _ r 0):asms) = opt (ZeroR () r:asms)
