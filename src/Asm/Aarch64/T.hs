8d52c547cf0008bed4d7a6498e498e6e2ee4d2b2
Vanessa McHale <vamchale@gmail.com>
2024-10-21 18:03:19 -0400

    cases

--- src/Asm/Aarch64/T.hs
+++ src/Asm/Aarch64/T.hs
@@ -110,6 +110,8 @@ ir (IR.S2 Op.FPlus t r) = pure [Faddp () (fabsReg t) (f2absReg r)]
 ir (IR.S2 Op.FMax t r) = pure [Fmaxp () (fabsReg t) (f2absReg r)]
 ir (IR.S2 Op.FMin t r) = pure [Fminp () (fabsReg t) (f2absReg r)]
 ir (IR.Fill2 r t) = pure [DupD () (f2absReg r) (fabsReg t)]
+ir (IR.CD (IR.AP rB Nothing _)) = pure [Prfm () (Pfop PLD L1 Keep) (R (absReg rB))]
+ir (IR.CD (IR.AP rB (Just (IR.ConstI i)) _)) = pure [Prfm () (Pfop PLD L1 Keep) (RP (absReg rB) (fromIntegral i))]
 ir (IR.CD (IR.AP rB (Just (IR.IB Op.IAsl eI (IR.ConstI 3))) _)) = do
     (plE,i) <- plI eI
     pure $ plE [Prfm () (Pfop PLD L1 Keep) (BI (absReg rB) i Three) ]
