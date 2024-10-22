5bc7326b2390141f0992769cf118c85fb7146104
Vanessa McHale <vamchale@gmail.com>
2024-10-22 07:23:47 -0400

    sty

--- include/apple_p.h
+++ include/apple_p.h
@@ -9,7 +9,7 @@ void paf(U xs) {
     J* dims=xs;
     J rnk=dims[0];dims+=1;
     pj(rnk);pf(" ");
-    J t=1;J d;
+    J t=1,d;
     DO(i,rnk,{d=dims[i];t*=d;pj(d);if (i!=rnk-1) {pf(",");}}) nl;
     F* e=xs+(rnk+1)*sizeof(F);
     PA("%f",t,e);
@@ -19,8 +19,18 @@ void pai(U xs) {
     J* dims=xs;
     J rnk=dims[0];dims+=1;
     pj(rnk);pf(" ");
-    J t=1;J d;
+    J t=1,d;
     DO(i,rnk,{d=dims[i];t*=d;pj(d);if (i!=rnk-1) {pf(",");}}) nl;
     J* e=xs+(rnk+1)*sizeof(J);
     PA("%lld",t,e);
+}
+
+void pab(U xs) {
+    J* dims=xs;
+    J rnk=dims[0];dims+=1;
+    pj(rnk);pf(" ");
+    J t=1,d;
+    DO(i,rnk,{d=dims[i];t*=d;pj(d);if(i!=rnk-1){pf(",");}}) nl;
+    B* e=xs+(rnk+1)*8;
+    PA("%d",t,e);
 }
