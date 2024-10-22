0ed98634f91d6bf924a243846c110f80c1ce5cd0
Vanessa McHale <vamchale@gmail.com>
2024-10-21 20:48:51 -0400

    nah

--- src/C.hs
+++ src/C.hs
@@ -200,8 +200,6 @@ data CS a = For { lann :: a, ixVar :: Temp, eLow :: CE, loopCond :: IRel, eUpper
           | FRnd { lann :: a, frndDest :: FTemp }
           | Def { lann :: a, fLabel :: Label, body :: [CS a] }
           | G { lann :: a, gt :: Label, retLabel :: Label }
-          | CD { lann :: a, dtgt :: ArrAcc } | CS { lann :: a, stgt :: ArrAcc }
-          | WS { lann :: a, stgt :: ArrAcc }
           deriving Functor
           -- TODO: PlDims cause we have diml
 
@@ -256,9 +254,6 @@ pL f (Def la l cs)         = hardline <> pS l <> ":" <#> indent 4 (pCS f cs) <> 
 pL f (G la l _)            = "GOTO" <+> pS l <> f la
 pL f (Comb l s t r)        = parens ("combine" <> pretty s <+> pretty t <> "," <+> pretty r) <> f l
 pL f (Fill l r t)          = parens ("fill" <+> pretty r <+> brackets (pretty t)) <> f l
-pL f (CD l a) = parens ("prep" <+> pretty a) <> f l
-pL f (CS l a) = parens ("prep-s" <+> pretty a) <> f l
-pL f (WS l a) = parens ("wprep-s" <+> pretty a) <> f l
 
 pS :: Label -> Doc ann
 pS l = "fun_" <> pretty l
--- src/IR.hs
+++ src/IR.hs
@@ -87,7 +87,6 @@ data Stmt = L Label
           | Cpy1 AE AE Exp
           | C Label | R Label
           | IRnd Temp | FRnd FTemp
-          | CD AE | CS AE | WS AE
 
 instance Pretty Stmt where
     pretty (L l)         = hardline <> prettyLabel l <> ":"
@@ -118,9 +117,6 @@ instance Pretty Stmt where
     pretty (Cset t e)    = parens ("cset" <+> pretty t <+> "<-" <+> pretty e)
     pretty (S2 o t r)    = parens ("comb" <> pretty o <+> pretty t <+> pretty r)
     pretty (Fill2 r t)   = parens ("fill" <+> pretty r <> "," <+> pretty t)
-    pretty (CD a)        = parens ("pre" <+> pretty a)
-    pretty (CS a)        = parens ("pre-s" <+> pretty a)
-    pretty (WS a)        = parens ("wpre-s" <+> pretty a)
 
 instance Show Stmt where show = show . pretty
 
