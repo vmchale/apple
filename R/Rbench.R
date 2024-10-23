c6a334ddf15edc71e380adfc2719806115dcc5e7
Vanessa McHale <vamchale@gmail.com>
2024-10-22 23:00:15 -0400

    faster lol

--- R/Rbench.R
+++ R/Rbench.R
@@ -7,3 +7,9 @@ sliding_mean<-jit("([((+)/x)%ℝ(:x)]⑄7)")
 microbenchmark(rollmean(a,7))
 microbenchmark(rollapply(a,7,(mean)))
 microbenchmark(run(sliding_mean,a))
+
+x<-runif(10000,0,1);y<-runif(10000,0,1)
+microbenchmark(x%*%y)
+
+dp<-jit("[(+)/ ((*)`(x::Vec n float) y)]")
+microbenchmark(run(dp,x,y))
