250d59f917d8720b55ac6685699932be5659347c
Vanessa McHale <vamchale@gmail.com>
2024-10-25 21:14:53 -0400

    faster?

--- R/Rbench.R
+++ R/Rbench.R
@@ -7,24 +7,3 @@ sliding_mean<-jit("([((+)/x)%ℝ(:x)]⑄7)")
 microbenchmark(rollmean(a,7))
 microbenchmark(rollapply(a,7,(mean)))
 microbenchmark(run(sliding_mean,a))
-
-x<-runif(10000,0,1);y<-runif(10000,0,1)
-microbenchmark(x%*%y)
-microbenchmark(sum(x*y))
-
-dp<-jit("[(+)/ ((*)`(x::Vec n float) y)]")
-microbenchmark(run(dp,x,y))
-
-A<-matrix(runif(1024,0,1),32);x<-runif(32,0,1)
-vmul<-jit("[x::M float%:y]")
-microbenchmark(A%*%x)
-microbenchmark(run(vmul,A,x))
-
-B<-matrix(runif(4096,0,1),64);C<-matrix(runif(4096,0,1),64)
-m6<-jit("[(x::(Arr (64×64) float))%.(y::Arr (64×64) float)]")
-microbenchmark(B%*%C)
-microbenchmark(run(m6,B,C))
-
-mT6<-jit("[(x::(Arr (64×64) float))%.|:(y::Arr (64×64) float)]")
-microbenchmark(B%*%t(C))
-microbenchmark(run(mT6,B,C))
---
+++ R/mat.R
@@ -0,0 +1,28 @@
+library(microbenchmark)
+source("./apple.R")
+
+x<-runif(10000,0,1);y<-runif(10000,0,1)
+microbenchmark(x%*%y)
+microbenchmark(sum(x*y))
+
+dp<-jit("[(+)/ ((*)`(x::Vec n float) y)]")
+microbenchmark(run(dp,x,y))
+
+A<-matrix(runif(1024,0,1),32);x<-runif(32,0,1)
+vmul<-jit("[x::M float%:y]")
+microbenchmark(A%*%x)
+microbenchmark(run(vmul,A,x))
+
+library(glue)
+msz <- function(M,N,K){
+    A<-matrix(runif(M*N,0,1),M);B<-matrix(runif(N*K,0,1),K)
+    m<-jit(glue::glue("[(x::(Arr ({M}×{N}) float))%.(y::Arr ({N}×{K}) float)]"))
+    print(microbenchmark(A%*%B));print(microbenchmark(run(m,A,B)))
+}
+
+mT <- function(M,N,K){
+    A<-matrix(runif(M*N,0,1),M);B<-matrix(runif(K*N,0,1),N)
+    mT<-jit(glue::glue("[(x::(Arr ({M}×{N}) float))%.|:(y::Arr ({K}×{N}) float)]"))
+    print(microbenchmark(A%*%t(B)));print(microbenchmark(run(mT,A,B)))
+}
+msz(64,64,64);mT(64,64,64);mT(1024,1024,1024)
