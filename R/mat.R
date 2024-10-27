661827fd3ebb3f09d43080bad43483ea238e3d0e
Vanessa McHale <vamchale@gmail.com>
2024-10-27 12:53:07 -0400

    dif

--- R/mat.R
+++ R/mat.R
@@ -15,14 +15,16 @@ microbenchmark(run(vmul,A,x))
 
 library(glue)
 msz <- function(M,N,K){
+    print(c(M,N,K))
     A<-matrix(runif(M*N,0,1),M);B<-matrix(runif(N*K,0,1),K)
     m<-jit(glue::glue("[(x::(Arr ({M}×{N}) float))%.(y::Arr ({N}×{K}) float)]"))
-    print(microbenchmark(A%*%B));print(microbenchmark(run(m,A,B)))
+    print(microbenchmark(A%*%B))
 }
 
 mT <- function(M,N,K){
+    print(c(M,N,K))
     A<-matrix(runif(M*N,0,1),M);B<-matrix(runif(K*N,0,1),N)
     mT<-jit(glue::glue("[(x::(Arr ({M}×{N}) float))%.|:(y::Arr ({K}×{N}) float)]"))
-    print(microbenchmark(A%*%t(B)));print(microbenchmark(run(mT,A,B)))
+    print(microbenchmark(run(mT,A,B)));print(microbenchmark(A%*%t(B)))
 }
-msz(64,64,64);mT(64,64,64);mT(1024,1024,1024)
+mT(64,64,64);msz(64,64,64);mT(512,512,512);mT(1024,1024,1024)
