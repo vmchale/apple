c998cb92899e3125ad6660942c735f31abf0713a
Vanessa McHale <vamchale@gmail.com>
2024-11-07 07:51:54 -0500

    works in theory lol

--- src/C/Alloc.hs
+++ src/C/Alloc.hs
@@ -4,17 +4,19 @@ import           C
 import           C.CF
 import           CF
 import           CF.AL
-import           Control.Monad.Trans.State.Strict (State, evalState, modify, state)
-import           Data.Functor                     (($>))
+import           Control.Composition              ((.*))
+import           Control.Monad.Trans.State.Strict (State, gets, runState, state)
+import           Data.Bifunctor                   (second)
 import qualified Data.IntMap                      as IM
 import qualified Data.IntSet                      as IS
 import           Data.List                        (find)
 import           Data.Maybe                       (mapMaybe)
+import           Data.Tuple                       (swap)
 import           LR
 import           Sh
 
 frees :: IM.IntMap Temp -> [CS ()] -> [CS Liveness]
-frees a = iF a.live.raa a.live
+frees a = (\(n,cs) -> iF n a cs).second live.raa a.live
 
 live :: [CS a] -> [CS Liveness]
 live = fmap (fmap liveness) . (\(is,isns,lm) -> reconstruct is lm isns) . cfC
@@ -23,23 +25,20 @@ sus = error "Array only freed at the beginning of one branch of the conditional.
 
 type Alias = IM.IntMap Int
 
-data Slots = Ss { keep :: !Alias, mLive :: [(AL, Sh ())] }
-
-iK :: Int -> Int -> Slots -> Slots
-iK o n (Ss ms l) = Ss (IM.insert o n ms) l
+data Slots = Ss { keep :: !Alias, nfree :: !IS.IntSet, mLive :: [(AL, Sh ())] }
 
 (@@) :: Alias -> IS.IntSet -> IS.IntSet
 (@@) s = IS.map (\l -> IM.findWithDefault l l s)
 
 m'liven :: IS.IntSet -> AL -> Sh () -> State Slots (Maybe AL)
 m'liven il l@(AL i) sh = state g where
-    g s@(Ss k ls) =
+    g s@(Ss k m ls) =
         case ffit il s sh of
-            Nothing         -> (Nothing, Ss k ((l,sh):ls))
-            Just l'@(AL i') -> (Just l', Ss (IM.insert i i' k) ls)
+            Nothing         -> (Nothing, Ss k m ((l,sh):ls))
+            Just l'@(AL i') -> (Just l', Ss (IM.insert i i' k) (IS.insert i m) ls)
 
 ffit :: IS.IntSet -> Slots -> Sh () -> Maybe AL
-ffit il (Ss ms aaϵ) sh = fst <$> find (\(lϵ, sh') -> lϵ `notMember` (ms@@il) && fits sh sh') aaϵ
+ffit il (Ss ms _ aaϵ) sh = fst <$> find (\(lϵ, sh') -> lϵ `notMember` (ms@@il) && fits sh sh') aaϵ
 
 ilt :: I a -> I a -> Bool
 ilt (Ix _ i) (Ix _ j)                     = i <= j
@@ -58,25 +57,30 @@ fits _ _                              = False
 st :: IM.IntMap Temp -> AL -> Temp
 st k (AL i)= IM.findWithDefault (error "Internal error: bad substitution?") i k
 
-raa :: IM.IntMap Temp -> [CS Liveness] -> [CS Liveness]
-raa ts cs = evalState (aa ts cs) (Ss IM.empty [])
+raa :: IM.IntMap Temp -> [CS Liveness] -> (IS.IntSet, [CS Liveness])
+raa = swap . second nfree . flip runState (Ss IM.empty IS.empty []) .* aa
+
+sCF :: Alias -> Liveness -> Liveness
+sCF al (Liveness i o fi fo) = Liveness (al@@i) (al@@o) fi fo
 
 -- every time we encounter an allocation, make note (size via malloc).
 --
 -- first cut: don't do re-fills
 aa :: IM.IntMap Temp -> [CS Liveness] -> State Slots [CS Liveness]
-aa ts (c@(Ma a sh l@(AL i) t _ _ _):cs) = do
+aa ts (c@(Ma a sh l t _ _ _):cs) = do
     s <- m'liven (ins a) l sh
     next <- case s of
             Nothing         -> pure c
-            Just l'@(AL i') -> modify (iK i i') $> Aa a l t (st ts l') 0
+            Just l' -> do
+                al <- gets keep
+                pure $ Aa (sCF al a) l t (st ts l') 0
     (next:) <$> aa ts cs
-aa ts (c:cs) = (c:) <$> aa ts cs
+aa ts (c:cs) = do {s <- gets keep; (fmap (sCF s) c:) <$> aa ts cs}
 -- new liveness information needs to prevent frees oops
 aa _ [] = pure []
 
-iF :: IM.IntMap Temp -> [CS Liveness] -> [CS Liveness]
-iF a = gg where
+iF :: IS.IntSet -> IM.IntMap Temp -> [CS Liveness] -> [CS Liveness]
+iF m a = gg where
     gg (RA{}:cs)                             = gg cs
     gg [s@(For l _ _ _ _ cs)]                = s { body = gg cs }:fss l
     gg [s@(Rof l _ _ cs)]                    = s { body = gg cs }:fss l
@@ -109,6 +113,6 @@ iF a = gg where
     gEs l s x = if es l s then x else sus
 
     fss l = [ Free t | t <- tss l ]
-    tss l = mapMaybe (`IM.lookup` a) (IS.toList (ins l `IS.difference` out l))
+    tss l = mapMaybe (`IM.lookup` a) (IS.toList ((ins l IS.\\ out l) IS.\\ m))
     fs l0 s1 = [ Free t | t <- ts l0 s1 ]
-    ts l0 s1 = mapMaybe (`IM.lookup` a) (IS.toList (ins l0 `IS.difference` ins (lann s1)))
+    ts l0 s1 = mapMaybe (`IM.lookup` a) (IS.toList ((ins l0 IS.\\ ins (lann s1)) IS.\\ m))
