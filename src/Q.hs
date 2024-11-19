6c56b26c958481cd5532275fe3aaf566061335e0
Vanessa McHale <vamchale@gmail.com>
2024-11-19 17:26:46 -0500

    oop

--- src/Q.hs
+++ src/Q.hs
@@ -3,7 +3,7 @@ module Q ((@<>), (#.), (!:)) where
 import qualified Data.IntMap as IM
 
 infixr 7 @<>
-infixl 4 #.
+infixr 4 #.
 
 (@<>) :: (Monoid m, Foldable f) => (a -> m) -> f a -> m
 (@<>) = foldMap
