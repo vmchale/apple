70badf9aba38da3b35003cdd933a97a9e7b8ce34
Vanessa McHale <vamchale@gmail.com>
2024-12-13 16:08:10 -0500

    weeder

--- src/Nm/IntSet.hs
+++ src/Nm/IntSet.hs
@@ -1,4 +1,4 @@
-module Nm.IntSet ( singleton, insert, member ) where
+module Nm.IntSet ( singleton, insert ) where
 
 import qualified Data.IntSet as IS
 import           Nm
@@ -6,4 +6,3 @@ import           U
 
 singleton (Nm _ (U i) _) = IS.singleton i
 insert (Nm _ (U i) _) = IS.insert i
-member (Nm _ (U i) _) = IS.member i
