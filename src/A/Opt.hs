185ba18de73c101f8d44c00b72810da0a3a30550
Vanessa McHale <vamchale@gmail.com>
2025-01-03 16:46:50 -0500

    better

--- src/A/Opt.hs
+++ src/A/Opt.hs
@@ -80,7 +80,7 @@ optA (EApp l (Builtin _ Last) e) = do
     e' <- optA e
     case e' of
         (EApp _ (EApp _ (EApp _ (Builtin _ Gen) seed) f) n) -> pure $ Id l (Iter f seed n)
-        _                                                   -> optA $ Id l (Aɴ e [EApp I (Builtin undefined Dim) e `iMinus` ILit I 1])
+        _                                                   -> optA $ Id l (Aɴ e' [EApp I (Builtin undefined Dim) e' `iMinus` ILit I 1])
 optA (EApp l (EApp _ (Builtin _ A1) e) n) =
     optA $ Id l (Aɴ e [n])
 optA (Id l0 (Aɴ e ns)) = do
