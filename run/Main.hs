3f2af48f557f71974fba7700cede8d8e2b6d2717
Vanessa McHale <vamchale@gmail.com>
2025-01-03 15:06:43 -0500

    ixd

--- run/Main.hs
+++ run/Main.hs
@@ -253,7 +253,7 @@ langHelp = liftIO $ putStr $ concat
     , lOption "ug." "unfold" "(i × j)" "dimensions"
     , lOption "gen." "generate" "}:?" "init"
     , lOption "{:?" "tail" "∴" "compose"
-    , lOption "𝔸" "digit literal" "𝔹" "convert->bool"
+    , lOption "𝔸" "digit literal" "ᶥ" "vector indices"
     , lOption "〃" "ditto" "𝐒,𝐊" "combinators"
     , lOption "⑂" "fork" "𝞈,𝟘,𝟙,𝟚" "fancy types"
     ]
