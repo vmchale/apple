5cb6f79198b96d30ad836c09f9d0c393de1a53ee
Vanessa McHale <vamchale@gmail.com>
2025-01-04 06:28:35 -0500

    bell numbers :D

--- test/Spec.hs
+++ test/Spec.hs
@@ -75,6 +75,7 @@ allT = testGroup "jit"
     , testCase "part" $ do { res <- fpIv "math/oeis/A000041.🍏" 12; res @?= [1::Int64,1,2,3,5,7,11,15,22,30,42,56,77]}
     , testCase "catalan numbers" $ do { res <- fpIv "math/oeis/A000108.🍎" 6; res @?= [1::Int64,1,2,5,14,42,132,429]}
     , testCase "catalan" $ do { res <- fpIi "math/combinatorics/catalan.🍎" 10 ; res @?= 16796 }
+    , testCase "bell" $ do { res <- fpIv "math/combinatorics/bell.🍎" 6; res @?= [1::Int64,1,2,5,15,52]}
     , testCase "base" $ do { res <- fpIiv "examples/base.🍏" 15 3; res @?= [1,2,0::Int64] }
     , testCase "7-day sliding average" $ do { res <- fpVv "test/examples/weekMean.🍎" [0..7::Double] ; res @?= [3,4::Double] }
     , testCase "bessel1" $ do { res <- fpIff "math/bessel.🍏" 1 3 ; res @?= bessel1 1 3 }
