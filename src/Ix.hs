456a497402fe61a2ee36875590cb0b4e383f8562
Vanessa McHale <vamchale@gmail.com>
2025-01-09 15:33:26 -0500

    just in case?

--- src/Ix.hs
+++ src/Ix.hs
@@ -3,18 +3,16 @@ module Ix ( L (..), pr, pc, psh ) where
 import           Sh
 
 ip :: I a -> L
-ip (Ix _ i)          | even i = E
-                     | odd i = O
+ip (Ix _ i) | even i&&i>0 = E | odd i = O
 ip (StaPlus _ i0 i1) = sp (ip i0) (ip i1)
-ip (StaMul _ i0 i1)  = mp (ip i0) (ip i1)
-ip _                 = U
+ip (StaMul _ i0 i1) = mp (ip i0) (ip i1)
+ip _ = U
 
 pr,pc,psh :: Sh a -> L
 pr (i `Cons` _) = ip i; pr _ = U
 pc (_ `Cons` i `Cons` _) = ip i; pc _ = U
 psh (i `Cons` sh) = mp (ip i) (psh sh); psh _ = U
 
--- data D = Z | S
 data L = E | O | U
 
 sp,mp :: L -> L -> L
