f3eb7bf8bf026f5870474a691b70b89e9692d4bd
Vanessa McHale <vamchale@gmail.com>
2025-01-12 09:55:14 -0500

    infer

--- src/Ty.hs
+++ src/Ty.hs
@@ -418,6 +418,9 @@ mgSh f l s sh0@Cons{} sh1@(Cat shh shϵ) | (is, Nil) <- unroll sh0, (isϵ, Nil) 
               (_, s0) <- mgSh f l s shh (roll Nil ish)
               (_, s1) <- mgShPrep f l s0 (roll Nil isϵ') shϵ
               pure (sh0, s1)
+mgSh f l s sh0 (Cat sh1 sh2) | (i0, Nil) <- unroll sh0, (i1, shϵ) <- unroll sh2, length i1==length i0 = do
+    (_, s0) <- mgSh f l s sh1 Nil
+    mgShPrep f l s0 (roll Nil i0) (roll shϵ i1)
 mgSh f l s sh0@Cat{} sh1@Cons{} = mgSh f l s sh1 sh0
 mgSh _ l _ sh0@Cons{} sh1 = throwError $ UShD l sh0 sh1
 mgSh _ l _ sh0 sh1@Cons{} = throwError $ UShD l sh0 sh1
