bb0b5ab03f3d121cb71f2b874993f4b10ead70fd
Vanessa McHale <vamchale@gmail.com>
2025-01-22 10:56:06 -0500

    hope this is right

--- src/C/Trans.hs
+++ src/C/Trans.hs
@@ -394,7 +394,7 @@ vslop sz n = do
   where
     szSlop=16+fromIntegral n*sz
 
-idims rnk oRnk xR lX = [EAt (ADim xR (KI l) lX) | l <- take (fromIntegral rnk) [oRnk-1,oRnk-2..] ]
+idims rnk oRnk xR lX = [EAt (ADim xR (KI l) lX) | l <- reverse (take (fromIntegral rnk) [oRnk-1,oRnk-2..]) ]
 
 plSlop :: Int64 -> Int64 -> [CE] -> CM (Temp, Temp, [CS ()], CS ())
 plSlop sz slopRnk dims = do
@@ -1214,21 +1214,19 @@ aeval (EApp oTy@(Arr sh _) (EApp _ g@(EApp _ (Builtin _ Zip) op) xs) ys) t a | (
     pure (plEX$plEY$nR =: ev (eAnn xs) (aPX,lX):vSz sh t a (Tmp nR) zSz++[contents])
 aeval (EApp (Arr sh _) (EApp _ (EApp _ (Builtin _ Zip) op) xs) ys) t a
     | Arrow tX (Arrow tY tC) <- eAnn op
-    , tYs@(Arr ySh _) <- eAnn ys, (Arr xSh _) <- eAnn xs
+    , tYs@(Arr ySh _) <- eAnn ys, tXs@(Arr xSh _) <- eAnn xs
     , Just (tXE, slopRnk) <- tRnk tX
     , Just szX <- nSz tXE, Just szY <- nSz tY, Just szC <- nSz tC
     , Just rnk <- staRnk sh, Just yRnk <- staRnk ySh, Just xRnk <- staRnk xSh = do
     nR <- nI
     y <- rtemp tY; z <- rtemp tC
     (plX, (lX, xsR)) <- plA xs; (plY, (lY, ysR)) <- plA ys
-    (slopP, slopN, aSlop, pops) <- do
-        (dtxs,dxss) <- plDim slopRnk (xsR,lX)
-        third4 (dxss++) <$> plSlop szX slopRnk (Tmp<$>dtxs)
+    (slopP, slopN, aSlop, pops) <- plSlop szX slopRnk (idims slopRnk xRnk xsR lX)
     (dts,dss) <- plDim yRnk (ysR, lY)
     m <- mdn sh t a rnk dts szC
     (_, ss) <- writeF op [AA slopP Nothing, ra y] z
     loop <- afor sh 0 ILt (Tmp nR) $ \k -> cpy (AElem slopP (KI slopRnk) Nothing 0) (AElem xsR (KI xRnk) lX (Tmp k*Tmp slopN)) (Tmp slopN) szX:mt (AElem ysR (KI yRnk) lY (Tmp k) szY) y:ss++[wt (AElem t (KI rnk) (Just a) (Tmp k) szC) z]
-    pure (plX$plY$nR=:ev tYs (ysR, lY):dss++m++aSlop++[loop, pops])
+    pure (plX$plY$nR=:ev tXs (xsR, lX):dss++m++aSlop++[loop, pops])
     | otherwise = unsupported
 aeval (EApp (Arr oSh _) (EApp _ g@(EApp _ (Builtin _ ScanS) op) seed) e) t a | (Arrow tX (Arrow tY _)) <- eAnn op, Just xSz <- rSz tX, nind tY = do
     acc <- rtemp tX; n <- nI
@@ -2130,7 +2128,6 @@ gpt (TΠ rs)=rs
 
 unsupported = error "Requires statically known rank."
 
-third4 f ~(x,y,z,w) = (x,y,f z,w)
 qmap f g h k ~(x,y,z,w) = (f x, g y, h z, k w)
 
 nyi e = error ("Not yet implemented: " ++ show e)
