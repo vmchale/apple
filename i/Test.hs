b0563077cb9bd35875cfeaac2036d2af72ce67ac
Vanessa McHale <vamchale@gmail.com>
2025-01-27 14:59:14 -0500

    nah

--- i/Test.hs
+++ i/Test.hs
@@ -1,12 +1,9 @@
 module Main (main) where
 
-import           Control.Exception                (bracket)
 import           Control.Monad.Trans.State.Strict (evalStateT)
 import qualified Data.ByteString.Lazy.Char8       as ASCIIL
-import           GHC.IO.Handle                    (hDuplicate, hDuplicateTo)
 import           REPL
 import           System.Console.Haskeline         (defaultSettings, runInputTBehavior, useFile)
-import           System.IO                        (IOMode (ReadMode), hClose, hFlush, hPutStr, openFile, stdin)
 import           System.IO.Silently               (capture_)
 import           Test.Tasty                       (defaultMain, testGroup)
 import           Test.Tasty.Golden                (goldenVsString)
