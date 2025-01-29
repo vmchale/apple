00647c4fa760c4b0cfc95aefa25bc61f468fb542
Vanessa McHale <vamchale@gmail.com>
2025-01-29 06:17:14 -0500

    sort in-place (currently wrong)

--- src/C/Trans.hs
+++ src/C/Trans.hs
@@ -597,48 +597,32 @@ aeval (EApp (Arr oSh _) g@(Builtin _ AddDim) xs) t a | (Arr sh ty) <- eAnn xs, J
     contents <- rfill g (AD t (Just a) Nothing (Just$Tmp rnk) (Just sz) Nothing) [AI (AD xR lX Nothing (Just$Tmp xRnk) Nothing (Just$Tmp szR))]
     pure (plX$xRnk=:eRnk sh (xR,lX):SZ () szR xR (Tmp xRnk) lX:rnk =: (Tmp xRnk+1):Ma () oSh a t (Tmp rnk) (Tmp szR) sz:
            [Wr () (ADim t 0 (Just a)) 1, CpyD () (ADim t 1 (Just a)) (ADim xR 0 lX) (Tmp xRnk)]++contents)
-aeval (EApp oTy@(Arr oSh tX) (Builtin _ Sort) x) t a | Just lt <- cr tX = do
+aeval (EApp oTy@(Arr oSh tX) (Builtin _ Sort) x) t a | Just c <- cr tX = do
     (plX, (lX, xR)) <- plA x
-    ph <- nI; bl <- nI; nB <- nI; i₀ <- nI; i₁ <- nI; iₒ <- nI; bl₀ <- nI; bl₁ <- nI
-    e₀ <- rtemp tX; e₁ <- rtemp tX
-    n <- nI; np <- nI; pad <- nI; blSz <- nI; blOSz <- nI; steps <- nI; i <- nI
-    slop <- nI; lS <- nextArr slop
-    inP <- nI; oP <- nI
-    let iAt0=Raw inP (Tmp bl₀*Tmp blSz+Tmp i₀) (Just lS) 8; iAt1=Raw inP (Tmp bl₁*Tmp blSz+Tmp i₁) (Just lS) 8
-        iAtO=Raw oP (Tmp bl*Tmp blOSz+Tmp iₒ) (Just lS) 8
-    pure (plX$
-         n=:ev oTy (xR,lX)
-        :steps=:(63-IU Clz (Tmp n)):np=:Bin IAsl 2 (Tmp steps):Cmov () (IRel INeq (Tmp n) (Tmp np)) steps (Tmp steps+1)
-        :pad=:(Tmp np-Tmp n)
-        -- pad it to a power of 2
-        :MaB () lS slop (Tmp np*(Tmp steps+1)*8)
-        :For () 1 i 0 ILt (Tmp pad) [ε (Raw slop (Tmp i) (Just lS) 8)]
-        :cpy (Raw slop (Tmp pad) (Just lS)) (AElem xR 1 lX 0) (Tmp n) 8
-        :i₀=:0:i₁=:0:inP=:Tmp slop:oP=:(Tmp slop+Tmp np*8):blSz=:1:blOSz=:2:nB=:Bin IAsr (Tmp np) 1
-        :For () 1 ph 1 ILeq (Tmp steps)
-            [ For () 1 bl 0 ILt (Tmp nB) [
-                i₀=:0, i₁=:0, bl₀=:(Tmp bl*2), bl₁=:(Tmp bl₀+1),
-                -- fill out-block/next slab
-                For () 1 iₒ 0 ILt (Tmp blOSz)
-                    [ If () (IRel IGeq (Tmp i₀) (Tmp blSz))
-                        [ Wr () iAtO (EAt iAt1), i₁+=1 ]
-                        [ If () (IRel IGeq (Tmp i₁) (Tmp blSz))
-                          [ Wr () iAtO (EAt iAt0), i₀+=1 ]
-                          [ mt iAt0 e₀, mt iAt1 e₁
-                          , If () (lt e₀ e₁) [wt iAtO e₀, i₀+=1] [wt iAtO e₁, i₁+=1]
-                          ]
-                        ]
+    e₀ <- rtemp tX; e₁ <- rtemp tX; ee <- rtemp tX
+    -- § 5.2.2 Knuth
+    ɴ <- nI; lɴ <- nI; tϵ <- nI; p <- nI; q <- nI; r <- nI; d <- nI; i <- nI
+    let l2 = [lɴ=:(63-(IU Clz (Tmp ɴ))), Cmov () (IRel INeq (Tmp ɴ) (Bin IAsl 2 (Tmp lɴ))) lɴ (Tmp lɴ+1)]
+        eat k = AElem t 1 (Just a) k 8
+        m3 = For () 1 i 0 ILeq (Tmp ɴ-Tmp d)
+                [ If () (IRel IEq (Bin (BI AndB) (Tmp i) (Tmp p)) (Tmp r))
+                    [ mt (eat (Tmp i)) e₀, mt (eat (Tmp i+Tmp d)) e₁
+                    , If () (c e₀ e₁) [mvt ee e₁, wt (eat (Tmp i)) ee, mvt ee e₀, wt (eat (Tmp i+Tmp d)) ee] []
                     ]
+                    []
                 ]
-            , inP=:Tmp oP, oP+=(Tmp np*8)
-            , blSz=:(Tmp blSz*2), blOSz=:(Tmp blOSz*2)
-            , nB=:Bin IAsr (Tmp nB) 1
+        loop = While () p IGt 0
+            [ q=:Tmp tϵ, r=:0, d=:Tmp p
+            , m3
+            , While () q INeq (Tmp p) [d=:(Tmp q-Tmp p), sr q, r=:Tmp p, m3]
+            , sr p
             ]
-        :v8 oSh t a (Tmp n)
-        ++[cpy (AElem t 1 (Just a) 0) (Raw slop (Tmp np*Tmp steps+Tmp pad) (Just lS)) (Tmp n) 8])
+    pure (plX$ɴ=:ev oTy (xR,lX):md oSh t a 1 (Tmp ɴ) [Tmp ɴ] 8++cpy (Raw t 0 (Just a)) (Raw xR 0 lX) (Tmp ɴ+2) 8
+          :l2++tϵ=:(Bin Op.IAsl 2 (Tmp lɴ-1)):p=:Tmp tϵ:[loop])
   where
-    cr I=Just (\(IT r0) (IT r1) -> IRel ILt (Tmp r0) (Tmp r1)); cr F=Just (\(FT x0) (FT x1) -> FRel FLt (FTmp x0) (FTmp x1)); cr _=Nothing
-    ε at=case tX of F -> WrF () at (let (_,ub)=floatRange (undefined::Double) in ConstF (- (encodeFloat (2^12-1) ub))); I -> Wr () at (KI minBound)
+    mvt (IT r0) (IT r1) = r0=:Tmp r1; mvt (FT r0) (FT r1) = MX () r0 (FTmp r1)
+    cr I=Just (\(IT r0) (IT r1) -> IRel IGt (Tmp r0) (Tmp r1)); cr F=Just (\(FT x0) (FT x1) -> FRel FGt (FTmp x0) (FTmp x1)); cr _=Nothing
+    sr r=r=:(Bin Op.IAsr (Tmp r) 1)
 aeval (EApp oTy@(Arr oSh _) e@(Builtin _ Init) x) t a | Just sz <- aB oTy = do
     nR <- nI
     (plX, (lX, xR)) <- plA x
