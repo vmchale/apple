5907429703b026310b9efafec2625fbec3c6f9fd
Vanessa McHale <vamchale@gmail.com>
2025-01-30 07:33:36 -0500

    more readable

--- R/Rbench.R
+++ R/Rbench.R
@@ -3,7 +3,7 @@ library(microbenchmark)
 a<-seq(0,999,1.0)
 source("./apple.R")
 
-sliding_mean<-jit("([((+)/x)%ℝ(:x)]⑄7)")
+sliding_mean<-jit("([(+)/x%ℝ(:x)]⑄7)")
 microbenchmark(rollmean(a,7))
 microbenchmark(rollapply(a,7,(mean)))
 microbenchmark(run(sliding_mean,a))
