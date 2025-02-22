c79eaeb14c18692814205ccaa07f2ebd2c40bfe8
Vanessa McHale <vamchale@gmail.com>
2025-02-22 09:04:53 -0500

    wait that was unused?? lol

--- pyc/apple.c
+++ pyc/apple.c
@@ -29,7 +29,7 @@ ZU b_npy(K NP o) {CT(o,'?',"Error: expected an array of booleans");A(rnk,n,1,x,o
 ZU i_npy(K NP o) {CT(o,'l',"Error: expected an array of 64-bit integers");A(rnk,n,8,x,o);J* x_i=x;U data=PyArray_DATA(o);memcpy(x_i+rnk+1,data,n*8);R x;}
 
 // https://stackoverflow.com/a/52737023/11296354
-#define RP(rnk,x,n,w,ls,T) {S sz=w*n;PY cap=PyCapsule_New(x,NULL,c_free);PyArray_Descr* pd=PyArray_DescrFromType(T); PY r=PyArray_NewFromDescr(&PyArray_Type,pd,(int)rnk,ls,NULL,x+rnk*8+8,NPY_ARRAY_C_CONTIGUOUS,NULL);PyArray_SetBaseObject((NP)r,cap);R r;}
+#define RP(rnk,x,n,w,ls,T) {PY cap=PyCapsule_New(x,NULL,c_free);PyArray_Descr* pd=PyArray_DescrFromType(T); PY r=PyArray_NewFromDescr(&PyArray_Type,pd,(int)rnk,ls,NULL,x+rnk*8+8,NPY_ARRAY_C_CONTIGUOUS,NULL);PyArray_SetBaseObject((NP)r,cap);R r;}
 
 #define NPA(f,s,T) _ PY f(U x) {CD(rnk,x,t,ls);RP(rnk,x,t,s,ls,T);}
 
