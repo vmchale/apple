d36017175d794c23dee5f5dbf1e9569752572652
Vanessa McHale <vamchale@gmail.com>
2025-03-21 11:51:48 -0400

    hlint

--- src/Ty.hs
+++ src/Ty.hs
@@ -962,7 +962,7 @@ satisfies (l,t) = listToMaybe . mapMaybe s . S.toList
 
 tyClosed :: Int -> E a -> Either (TyE a) (E (T ()), Int)
 tyClosed u e = do
-    ((eS), i) <- runTyM u (do {(e', s) <- tyE mempty e; pure (rwArr.(s@@)<$>e')})
+    (eS, i) <- runTyM u (do {(e', s) <- tyE mempty e; pure (rwArr.(s@@)<$>e')})
     chkE (eAnn eS) $> (eS, i)
 
 tyE :: Subst a -> E a -> TyM a (E (T ()), Subst a)
