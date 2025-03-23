1791e96a589dc8ab201d9f614f3005e573238ef3
Vanessa McHale <vamchale@gmail.com>
2025-03-23 15:26:37 -0400

    I guess that worked

--- src/Nm.hs
+++ src/Nm.hs
@@ -13,9 +13,6 @@ data Nm a = Nm { name   :: T.Text
 instance Eq (Nm a) where
     (==) (Nm _ u _) (Nm _ u' _) = u == u'
 
-instance Ord (Nm a) where
-    compare (Nm _ u _) (Nm _ u' _) = compare u u'
-
 instance Pretty (Nm a) where
     pretty (Nm n _ _) = pretty n
     -- pretty (Nm n (U i) _) = pretty n <> pretty i
