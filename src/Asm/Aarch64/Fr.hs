20055b6db6e36494c2ef4b85a6cb12e498c5c3b7
Vanessa McHale <vamchale@gmail.com>
2025-04-13 09:10:40 -0400

    compact

--- src/Asm/Aarch64/Fr.hs
+++ src/Asm/Aarch64/Fr.hs
@@ -38,28 +38,17 @@ frameC = concat.go IS.empty IS.empty IS.empty
           handleX0 Malloc=filter (/=X0); handleX0 Free=id; handleX0 Exp=id; handleX0 Log=id; handleX0 Pow=id; handleX0 DR=id; handleX0 JR=filter (/=X0)
           handleD0 Exp=filter (/=D0); handleD0 Log=filter (/=D0);handleD0 Malloc=id;handleD0 Free=id; handleD0 Pow=filter (/=D0); handleD0 DR=filter (/=D0); handleD0 JR=id
 
+-- Z0-Z7, P0-P3 argument/results
+-- Z8-Z15, P4-P15 callee-saved
+-- Z16-Z31 caller-saved
+
 -- https://developer.arm.com/documentation/102374/0102/Procedure-Call-Standard
 fromInt :: Int -> Maybe AReg
-fromInt 0     = Just X0
-fromInt 1     = Just X1
-fromInt 2     = Just X2
-fromInt 3     = Just X3
-fromInt 4     = Just X4
-fromInt 5     = Just X5
-fromInt 6     = Just X6
-fromInt 7     = Just X7
-fromInt (-1)  = Just X8
-fromInt (-2)  = Just X9
-fromInt (-3)  = Just X10
-fromInt (-4)  = Just X11
-fromInt (-5)  = Just X12
-fromInt (-6)  = Just X13
-fromInt (-7)  = Just X14
-fromInt (-8)  = Just X15
-fromInt (-9)  = Just X16
-fromInt (-10) = Just X17
-fromInt (-11) = Just X18
-fromInt _     = Nothing
+fromInt 0     = Just X0;  fromInt 1     = Just X1;  fromInt 2     = Just X2;  fromInt 3     = Just X3
+fromInt 4     = Just X4;  fromInt 5     = Just X5;  fromInt 6     = Just X6;  fromInt 7     = Just X7
+fromInt (-1)  = Just X8;  fromInt (-2)  = Just X9;  fromInt (-3)  = Just X10; fromInt (-4)  = Just X11
+fromInt (-5)  = Just X12; fromInt (-6)  = Just X13; fromInt (-7)  = Just X14; fromInt (-8)  = Just X15
+fromInt (-9)  = Just X16; fromInt (-10) = Just X17; fromInt (-11) = Just X18; fromInt _     = Nothing
 
 -- https://learn.microsoft.com/en-us/cpp/build/arm64-windows-abi-conventions?view=msvc-170#floating-pointsimd-registers
 fi 10 = D0; fi 11 = D1; fi 12 = D2; fi 13 = D3
@@ -72,28 +61,10 @@ fi (-39) = D24; fi (-40) = D25; fi (-41) = D26; fi (-42) = D27
 fi (-43) = D28; fi (-44) = D29; fi (-45) = D30; fi (-46) = D31
 
 fInt :: Int -> Maybe FAReg
-fInt 10    = Just D0
-fInt 11    = Just D1
-fInt 12    = Just D2
-fInt 13    = Just D3
-fInt 14    = Just D4
-fInt 15    = Just D5
-fInt 16    = Just D6
-fInt 17    = Just D7
-fInt (-31) = Just D16
-fInt (-32) = Just D17
-fInt (-33) = Just D18
-fInt (-34) = Just D19
-fInt (-35) = Just D20
-fInt (-36) = Just D21
-fInt (-37) = Just D22
-fInt (-38) = Just D23
-fInt (-39) = Just D24
-fInt (-40) = Just D25
-fInt (-41) = Just D26
-fInt (-42) = Just D27
-fInt (-43) = Just D28
-fInt (-44) = Just D29
-fInt (-45) = Just D30
-fInt (-46) = Just D31
+fInt 10    = Just D0;  fInt 11    = Just D1;  fInt 12    = Just D2;  fInt 13    = Just D3
+fInt 14    = Just D4;  fInt 15    = Just D5;  fInt 16    = Just D6;  fInt 17    = Just D7
+fInt (-31) = Just D16; fInt (-32) = Just D17; fInt (-33) = Just D18; fInt (-34) = Just D19
+fInt (-35) = Just D20; fInt (-36) = Just D21; fInt (-37) = Just D22; fInt (-38) = Just D23
+fInt (-39) = Just D24; fInt (-40) = Just D25; fInt (-41) = Just D26; fInt (-42) = Just D27
+fInt (-43) = Just D28; fInt (-44) = Just D29; fInt (-45) = Just D30; fInt (-46) = Just D31
 fInt _     = Nothing
