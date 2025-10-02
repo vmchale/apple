fbc7cc8f0212c93d1562734ad7b93c0b0c5c2f54
Vanessa McHale <vamchale@gmail.com>
2025-10-02 16:48:05 -0400

    not used I guess

--- src/Op.hs
+++ src/Op.hs
@@ -14,7 +14,7 @@ import           Prettyprinter (Pretty (..))
 data FUn = FSqrt | FLog | FSin | FCos | FAbs | FNeg
 
 data IP = IEven | IOdd
-data IUn = Clz | Cnt
+data IUn = Clz | IAbs
 
 data BUn = BNeg
 
@@ -68,4 +68,4 @@ instance Pretty FUn where
     pretty FNeg = "¬"
 
 instance Pretty IP where pretty IEven = "even"; pretty IOdd = "odd"
-instance Pretty IUn where pretty Clz = "clz"; pretty Cnt = "cnt"
+instance Pretty IUn where pretty Clz = "clz"; pretty IAbs = "abs"
