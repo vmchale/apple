01de424b5fa03b29194ea24056a73caeb89148d1
Vanessa McHale <vamchale@gmail.com>
2026-02-13 09:47:35 -0500

    boolean cases for completness/robust...

--- src/C/Trans.hs
+++ src/C/Trans.hs
@@ -1551,6 +1551,62 @@ peval (EApp _ (EApp _ (Builtin (Arrow (Arr _ ty) _) Eq) e0) e1) t | Arr sh _ <- 
 peval (EApp _ (EApp _ (Builtin _ op) e0) e1) t | Just boo <- mB op = do
     (pl0,e0R) <- plP e0; (pl1,e1R) <- plP e1
     pure $ pl0 $ pl1 [MB () t (Boo boo e0R e1R)]
+peval (EApp _ (EApp _ (Builtin (Arrow B _) Neq) e0) e1) t = do
+    (pl0,e0R) <- plP e0; (pl1,e1R) <- plP e1
+    pure $ pl0 $ pl1 [MB () t (Boo XorB e0R e1R)]
+peval (EApp _ (EApp _ (Builtin (Arrow B _) Gt) e0) e1) t = do
+    (pl0,e0R) <- plP e0; (pl1,e1R) <- plP (EApp B (Builtin (B~>B) N) e1)
+    pure $ pl0 $ pl1 [MB () t (Boo AndB e0R e1R)]
+peval (EApp _ (EApp _ (Builtin (Arrow B _) Gte) e0) e1) t = do
+    (pl0,e0R) <- plP e0; (pl1,e1R) <- plP (EApp B (Builtin (B~>B) N) e1)
+    pure $ pl0 $ pl1 [MB () t (Boo OrB e0R e1R)]
+peval (EApp _ (EApp _ (Builtin (Arrow B _) Lt) e0) e1) t = do
+    (pl0,e0R) <- plP (EApp B (Builtin (B~>B) N) e0); (pl1,e1R) <- plP e1
+    pure $ pl0 $ pl1 [MB () t (Boo AndB e0R e1R)]
+peval (EApp _ (EApp _ (Builtin (Arrow B _) Lte) e0) e1) t = do
+    (pl0,e0R) <- plP (EApp B (Builtin (B~>B) N) e0); (pl1,e1R) <- plP e1
+    pure $ pl0 $ pl1 [MB () t (Boo OrB e0R e1R)]
+peval (EApp _ (Builtin _ N) e0) t = do
+    (pl,e0R) <- plP e0
+    pure $ pl [MB () t (BU BNeg e0R)]
+peval (EApp _ (EApp _ (Builtin _ Fold) op) e) acc | tXs@(Arr xSh _) <- eAnn e, (Arrow tX _) <- eAnn op, isB tX = do
+    x <- nBT; szR <- nI
+    (plE, (l, aP)) <- plA e
+    ss <- writeRF op [PT acc, PT x] (PT acc)
+    loop <- afor1 xSh 1 ILt (Tmp szR) (\i -> MB () x (PAt (AElem aP 1 l (Tmp i) 1)):ss)
+    pure $ plE$szR =: ev tXs (aP,l):MB () acc (PAt (AElem aP 1 l 0 1)):[loop]
+peval (EApp _ (EApp _ (EApp _ (Builtin _ FoldS) op) seed) e) acc | (Arrow _ (Arrow tY _)) <- eAnn op, Just szY <- nSz tY = do
+    szR <- nI
+    (plE, (l, aP)) <- plA e
+    plAcc <- peval seed acc
+    (x, wX) <- arg tY (ve aP l szY)
+    ss <- writeRF op [PT acc, x] (PT acc)
+    loop <- afort tXs 0 ILt (Tmp szR) (\i -> wX i:ss)
+    pure $ plE $ plAcc++szR=:ev (eAnn e) (aP,l):[loop]
+  where
+    tXs=eAnn e
+peval (Id _ (U2 seeds gs c f n)) t | Just e <- traverse (rr.eAnn) seeds = do
+    plU <- peval c t
+    (plN,nE) <- plC n
+    k <- nI
+    xs <- traverse (rtemp.fst) e
+    plSeeds <- concat <$> zipWithM eeval seeds xs
+    usss <- concat <$> zipWithM (\g x -> writeRF g [x] x) gs xs
+    fss <- writeRF f (PT t:xs) (PT t)
+    pure $ plU ++ plN (plSeeds ++ [For () E.Z 1 k 0 ILt nE (fss++usss)])
+peval e@(EApp _ (Builtin _ TAt{}) Var{}) t = do
+    aa <- tat e
+    pure [MB () t (unBA aa)]
+peval (EApp _ (Builtin _ (TAt i)) (Tup _ es)) t = peval (es!!(i-1)) t
+peval (EApp _ (Builtin _ (TAt i)) e) t = do
+    (ss, as) <- plΠ e
+    pure (ss++[MB () t (unBA (as!!(i-1)))])
+peval (Id _ (FoldGen seed g f n)) t = do
+    x <- nBT; acc <- nBT; k <- nI
+    (plSeed,seedR) <- plBV seed; (plN,nE) <- plC n
+    uss <- writeRF g [PT x] (PT x)
+    fss <- writeRF f [PT acc, PT x] (PT acc)
+    pure $ plSeed $ plN ([MB () acc (Is seedR), MB () x (Is seedR)] ++ uss ++ [Rof () E.Z k (nE-1) (fss++uss), MB () t (Is acc)])
 peval (EApp _ (Builtin _ N) e0) t = do
     (pl,e0R) <- plP e0
     pure $ pl [MB () t (BU BNeg e0R)]
